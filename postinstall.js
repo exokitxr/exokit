6c330234b57ad79404fc6005595d8acc221fb0c7
Adrian Biedrzycki <a@modules.io>
2018-02-13 12:43:43 -0800

    Add missing mkdirp to postinstall script

--- postinstall.js
+++ postinstall.js
@@ -1,29 +1,36 @@
 const path = require('path');
 const fs = require('fs');
 const os = require('os');
+const mkdirp = require('mkdirp');
 
 const rootPath = path.join('/Users/', os.userInfo().username, '/Library/Application Support/OpenVR/.openvr');
 const openvrPathsPath = path.join(rootPath, 'openvrpaths.vrpath');
 fs.lstat(openvrPathsPath, (err, stats) => {
   if (err) {
     if (err.code === 'ENOENT') {
-      const jsonString = JSON.stringify({
-        "config" : [ rootPath ],
-        "external_drivers" : null,
-        "jsonid" : "vrpathreg",
-        "log" : [ rootPath ],
-        "runtime" : [
-           path.join(__dirname, '/node_modules/native-openvr-deps/bin/osx64'),
-         ],
-        "version" : 1
-      }, null, 2);
-      fs.writeFile(openvrPathsPath, jsonString, err => {
-        if (err) {
-          console.warn(err);
+      mkdirp(rootPath, err => {
+        if (!err) {
+          const jsonString = JSON.stringify({
+            "config" : [ rootPath ],
+            "external_drivers" : null,
+            "jsonid" : "vrpathreg",
+            "log" : [ rootPath ],
+            "runtime" : [
+               path.join(__dirname, '/node_modules/native-openvr-deps/bin/osx64'),
+             ],
+            "version" : 1
+          }, null, 2);
+          fs.writeFile(openvrPathsPath, jsonString, err => {
+            if (err) {
+              throw err;
+            }
+          });
+        } else {
+          throw err;
         }
       });
     } else {
-      console.warn(err);
+      throw err;
     }
   }
 });
