7ec9ad4e4876cd776b1ef719956266617ab5584c
Adrian Biedrzycki <a@modules.io>
2018-03-01 17:01:36 -0800

    Update exokit.sh to use local node only if available

--- exokit.sh
+++ exokit.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+NODE_BIN="node"
+if [ -e "node/node" ]; then
+  NODE_BIN="node/node"
+fi
+
 case "$OSTYPE" in
-  linux*) ./node_modules/isolator/lib/linux/isolator -- node/node . "$@" ;;
-  darwin*) ./node_modules/isolator/lib/macos/isolator -- node/node . "$@" ;; 
+  linux*) ./node_modules/isolator/lib/linux/isolator -- "$NODE_BIN" . "$@" ;;
+  darwin*) ./node_modules/isolator/lib/macos/isolator -- "$NODE_BIN" . "$@" ;;
   *) echo "Operating system not supported: $OSTYPE" ;;
 esac
