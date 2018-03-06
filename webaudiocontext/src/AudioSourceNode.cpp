7ef9e0a0015158cc709f6895fdf645cc05a1e232
Avaer Kazmer <a@modules.io>
2018-03-06 11:47:54 -0500

    Bugfix AudioSourceNode argument types detection

--- webaudiocontext/src/AudioSourceNode.cpp
+++ webaudiocontext/src/AudioSourceNode.cpp
@@ -8,17 +8,17 @@ AudioSourceNode::~AudioSourceNode() {}
 
 Handle<Object> AudioSourceNode::Initialize(Isolate *isolate) {
   Nan::EscapableHandleScope scope;
-  
+
   // constructor
   Local<FunctionTemplate> ctor = Nan::New<FunctionTemplate>(New);
   ctor->InstanceTemplate()->SetInternalFieldCount(1);
   ctor->SetClassName(JS_STR("AudioSourceNode"));
-  
+
   // prototype
   Local<ObjectTemplate> proto = ctor->PrototypeTemplate();
   AudioNode::InitializePrototype(proto);
   AudioSourceNode::InitializePrototype(proto);
-  
+
   Local<Function> ctorFn = ctor->GetFunction();
 
   return scope.Escape(ctorFn);
@@ -31,20 +31,32 @@ void AudioSourceNode::InitializePrototype(Local<ObjectTemplate> proto) {
 NAN_METHOD(AudioSourceNode::New) {
   Nan::HandleScope scope;
 
-  if (info[0]->IsObject() && info[0]->ToObject()->Get(JS_STR("constructor"))->ToObject()->Get(JS_STR("name"))->StrictEquals(JS_STR("Audio")) && info[1]->IsObject() && info[1]->ToObject()->Get(JS_STR("constructor"))->ToObject()->Get(JS_STR("name"))->StrictEquals(JS_STR("AudioContext"))) {
-    Audio *audio = ObjectWrap::Unwrap<Audio>(Local<Object>::Cast(info[0]));
+  if (info[1]->IsObject() && info[1]->ToObject()->Get(JS_STR("constructor"))->ToObject()->Get(JS_STR("name"))->StrictEquals(JS_STR("AudioContext"))) {
     Local<Object> audioContextObj = Local<Object>::Cast(info[1]);
 
-    AudioSourceNode *audioSourceNode = new AudioSourceNode();
-    Local<Object> audioSourceNodeObj = info.This();
-    audioSourceNode->Wrap(audioSourceNodeObj);
+    if (info[0]->IsObject() && info[0]->ToObject()->Get(JS_STR("constructor"))->ToObject()->Get(JS_STR("name"))->StrictEquals(JS_STR("HTMLAudioElement"))) {
+      Local<Object> htmlAudioElement = Local<Object>::Cast(info[0]);
+      Local<Value> audioValue = htmlAudioElement->Get(JS_STR("audio"));
 
-    audioSourceNode->context.Reset(audioContextObj);
-    audioSourceNode->audioNode = audio->audioNode;
+      if (audioValue->BooleanValue() && audioValue->IsObject() && audioValue->ToObject()->Get(JS_STR("constructor"))->ToObject()->Get(JS_STR("name"))->StrictEquals(JS_STR("Audio"))) {
+        Audio *audio = ObjectWrap::Unwrap<Audio>(Local<Object>::Cast(audioValue));
 
-    info.GetReturnValue().Set(audioSourceNodeObj);
+        AudioSourceNode *audioSourceNode = new AudioSourceNode();
+        Local<Object> audioSourceNodeObj = info.This();
+        audioSourceNode->Wrap(audioSourceNodeObj);
+
+        audioSourceNode->context.Reset(audioContextObj);
+        audioSourceNode->audioNode = audio->audioNode;
+
+        info.GetReturnValue().Set(audioSourceNodeObj);
+      } else {
+        Nan::ThrowError("AudioContext::CreateMediaElementSource: invalid audio element state");
+      }
+    } else {
+      Nan::ThrowError("AudioSourceNode: invalid media element");
+    }
   } else {
-    Nan::ThrowError("invalid arguments");
+    Nan::ThrowError("AudioSourceNode: invalid audio context");
   }
 }
 
