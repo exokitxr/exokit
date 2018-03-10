0d92b694b6877208bfe8e59497de676fd1bb71d9
Avaer Kazmer <a@modules.io>
2018-03-10 01:20:23 -0800

    Bugfix submodules checkout in preinstall script

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,19 +1,28 @@
 const path = require('path');
 const fs = require('fs');
 const child_process = require('child_process');
+const rimraf = require('rimraf');
+
+// if submodules were not checked out for some reason, manually check them out
 
 const rootPath = path.join(__dirname, '..');
-
 const submodulePaths = [
-  path.join(rootPath, 'deps', 'exokit-bindings'),
-  path.join(rootPath, 'deps', 'glfw'),
+  path.join(rootPath, 'deps', 'exokit-bindings', '.git'),
+  path.join(rootPath, 'deps', 'glfw', '.git'),
 ];
 
 if (submodulePaths.some(submodulePath => !fs.existsSync(submodulePath))) {
-  child_process.execFileSync('git', ['submodule', 'init'], {
+  const exokitBindingsPath = path.join(rootPath, 'deps', 'exokit-bindings');
+  rimraf.sync(exokitBindingsPath);
+  child_process.execFileSync('git', ['clone', '--depth=1', 'https://github.com/modulesio/exokit-bindings', exokitBindingsPath], {
     cwd: rootPath,
+    stdio: 'inherit',
   });
-  child_process.execFileSync('git', ['submodule', 'update'], {
+
+  const glfwPath = path.join(rootPath, 'deps', 'glfw');
+  rimraf.sync(glfwPath);
+  child_process.execFileSync('git', ['clone', '--depth=1', 'https://github.com/glfw/glfw', glfwPath], {
     cwd: rootPath,
+    stdio: 'inherit',
   });
 }
