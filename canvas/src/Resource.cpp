6d59a46ada6992d11d24b7c91bb6c8bb9c891cad
Avaer Kazmer <a@modules.io>
2018-03-15 02:34:22 -0400

    Bugfix DecodeDataToBitmap not defaulting to RGBA

--- canvas/src/Resource.cpp
+++ canvas/src/Resource.cpp
@@ -7,18 +7,18 @@
 
 #include <Resources.h>
 #include <SkBitmap.h>
-// #include "SkCommandLineFlags.h"
 #include <SkData.h>
 #include <SkImage.h>
 #include <SkImageGenerator.h>
-// #include <SkOSFile.h>
-// #include <SkOSPath.h>
-// #include <SkStream.h>
-// #include <SkTypeface.h>
 
 bool DecodeDataToBitmap(sk_sp<SkData> data, SkBitmap* dst) {
-    std::unique_ptr<SkImageGenerator> gen(SkImageGenerator::MakeFromEncoded(std::move(data)));
-    return gen && dst->tryAllocPixels(gen->getInfo()) &&
-        gen->getPixels(gen->getInfo().makeColorSpace(nullptr), dst->getPixels(), dst->rowBytes(),
-                       nullptr);
+  std::unique_ptr<SkImageGenerator> gen(SkImageGenerator::MakeFromEncoded(std::move(data)));
+  if (gen) {
+    SkImageInfo imageInfo = gen->getInfo()
+      .makeColorType(SkColorType::kRGBA_8888_SkColorType)
+      .makeColorSpace(nullptr);
+    return dst->tryAllocPixels(imageInfo) && gen->getPixels(imageInfo, dst->getPixels(), dst->rowBytes(), nullptr);
+  } else {
+    return false;
+  }
 }
