3fe2349f296a73a5065f33e345d6725dfa021555
Avaer Kazmer <a@modules.io>
2018-03-16 00:50:43 -0400

    Add new css color parser

--- canvas/include/web_color.h
+++ canvas/include/web_color.h
@@ -1,59 +1,39 @@
 #ifndef _WEB_COLOR_H_
 #define _WEB_COLOR_H_
 
-#include "web_string.h"
+#include "csscolorparser.h"
 
 using namespace std;
 
 namespace canvas
 {
-	struct def_color
-	{
-		const char*	name;
-		const char*	rgb;
-	};
+  struct web_color {
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+    unsigned char a;
 
-	extern def_color g_def_colors[];
+    web_color(unsigned char r, unsigned char g, unsigned char b, unsigned char a = 0xFF) : r(r), g(g), b(b), a(a) {}
 
-    class document_container;
+    web_color() : b(0), g(0), r(0), a(0xFF) {}
 
-	struct web_color
-	{
-		unsigned char b;
-		unsigned char g;
-		unsigned char r;
-		unsigned char a;
+    web_color(const web_color& val) {
+      this->r = val.r;
+      this->g = val.g;
+      this->b = val.b;
+      this->a = val.a;
+    }
 
-		web_color(unsigned char r, unsigned char g, unsigned char b, unsigned char a = 0xFF)
-		{
-			this->b = b;
-			this->g = g;
-			this->r = r;
-			this->a = a;
-		}
-
-		web_color() : b(0), g(0), r(0), a(0xFF) {}
-
-		web_color(const web_color& val)
-		{
-			this->b = val.b;
-			this->g = val.g;
-			this->r = val.r;
-			this->a = val.a;
-		}
-
-		web_color& operator=(const web_color& val)
-		{
-			this->b = val.b;
-		  this->g = val.g;
-			this->r = val.r;
-			this->a = val.a;
-			return *this;
-		}
-        static web_color            from_string(const char* str);
-		    static string    resolve_name(const char* name);
-        static bool                 is_color(const char* str);
-	};
+    web_color &operator=(const web_color& val)
+    {
+      this->r = val.r;
+      this->g = val.g;
+      this->b = val.b;
+      this->a = val.a;
+      return *this;
+    }
+    static web_color from_string(const char* str);
+  };
 }
 
 #endif
