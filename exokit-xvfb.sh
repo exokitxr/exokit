ff0edec78dd1324a96cd57eb2171e85d6e108a29
Avaer Kazmer <a@modules.io>
2018-04-20 04:19:53 -0400

    Bugfix exokit-xvfb.sh exokit.sh invokation cwd

--- exokit-xvfb.sh
+++ exokit-xvfb.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-xvfb-run -s "-screen 0 1280x1024x24" ./exokit.sh "$@"
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+xvfb-run -s "-screen 0 1280x1024x24" "$DIR/exokit.sh" "$@"
