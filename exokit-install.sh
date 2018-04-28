5fdd111ba20c570704b05ee9c081d626a7e0c969
Avaer Kazmer <a@modules.io>
2018-04-27 23:24:12 -0400

    Bugfix user detection in exokit-install.sh

--- exokit-install.sh
+++ exokit-install.sh
@@ -4,7 +4,7 @@
 # This script will download and install Exokit in /usr/lib
 
 echo "Exokit $VERSION install script"
-[ "$user" != 'root' ] && echo "This script must be run as root" && exit 1
+[ "$(whoami)" != 'root' ] && echo "This script must be run as root" && exit 1
 cd /tmp
 rm -Rf /usr/local/bin/exokit /usr/local/lib/exokit
 wget -qO- https://github.com/webmixedreality/exokit/releases/download/$VERSION/exokit-linux-bin.tar.gz | tar -zxf - --directory /usr/local
