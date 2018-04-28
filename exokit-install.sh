0248cf718bffce16ce01f061b530770997c38d29
Avaer Kazmer <a@modules.io>
2018-04-27 22:20:18 -0400

    Exokit install script tweaking for ash compatibility

--- exokit-install.sh
+++ exokit-install.sh
@@ -3,10 +3,7 @@
 # This script will download and install Exokit in /usr/lib
 
 echo "Exokit $VERSION install script"
-if [[ $EUID -ne 0 ]]; then
-  echo "This script must be run as root"
-  exit 1
-fi
+[ "$user" != 'root' ] && echo "This script must be run as root" && exit 1
 cd /tmp
 wget https://github.com/webmixedreality/exokit/releases/download/$VERSION/exokit-linux-full.tar.gz
 tar -zxf exokit-linux-full.tar.gz
