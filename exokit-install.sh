87df12fdef6466f81b27383586293bfb035802ab
Avaer Kazmer <a@modules.io>
2018-04-27 23:28:48 -0400

    Add progress bar to exokit-install.sh

--- exokit-install.sh
+++ exokit-install.sh
@@ -7,4 +7,4 @@ echo "Exokit $VERSION install script"
 [ "$(whoami)" != 'root' ] && echo "This script must be run as root" && exit 1
 cd /tmp
 rm -Rf /usr/local/bin/exokit /usr/local/lib/exokit
-wget -qO- https://github.com/webmixedreality/exokit/releases/download/$VERSION/exokit-linux-bin.tar.gz | tar -zxf - --directory /usr/local
+wget -O- https://github.com/webmixedreality/exokit/releases/download/$VERSION/exokit-linux-bin.tar.gz | tar -zxf - --directory /usr/local
