07c5625d931c83ce72e336aa3c99f9e0b7a9938e
Avaer Kazmer <a@modules.io>
2018-04-27 23:22:34 -0400

    More exokit-install.sh tweaking

--- exokit-install.sh
+++ exokit-install.sh
@@ -6,5 +6,5 @@
 echo "Exokit $VERSION install script"
 [ "$user" != 'root' ] && echo "This script must be run as root" && exit 1
 cd /tmp
-wget https://github.com/webmixedreality/exokit/releases/download/$VERSION/exokit-linux-full.tar.gz
-tar -zxf exokit-linux-full.tar.gz
+rm -Rf /usr/local/bin/exokit /usr/local/lib/exokit
+wget -qO- https://github.com/webmixedreality/exokit/releases/download/$VERSION/exokit-linux-bin.tar.gz | tar -zxf - --directory /usr/local
