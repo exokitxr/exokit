7a89657624ff6ca4ff092b6d263eb751f020160d
Avaer Kazmer <a@modules.io>
2018-04-27 22:04:36 -0400

    Add root detection to exokit install script again

--- exokit-install.sh
+++ exokit-install.sh
@@ -1,5 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 # wget https://get.webmr.io | sudo sh
 # This script will download and install Exokit in /usr/lib
 
-echo Install version VERSION
+echo "Exokit $VERSION install script"
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root"
+  exit 1
+fi
+cd /tmp
+wget https://github.com/webmixedreality/exokit/releases/download/$VERSION/exokit-linux-full.tar.gz
+tar -zxf exokit-linux-full.tar.gz
