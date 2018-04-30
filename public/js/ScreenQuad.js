51ded2a7b9249c8b6e99d74c74554f2ed34f9ec9
Avaer Kazmer <a@modules.io>
2018-04-30 05:17:24 -0400

    Make ScreenQuad derive uvs from position

--- public/js/ScreenQuad.js
+++ public/js/ScreenQuad.js
@@ -6,7 +6,7 @@ window.ScreenQuad = (() => {
 
 		"varying vec2 vUv;",
 		"void main(){",
-			"vUv = uv;",
+			"vUv = 0.5 + (position.xy / 2.);",
 			"gl_Position = vec4( position.xy , 1. , 1. );",
 		"}",
 
