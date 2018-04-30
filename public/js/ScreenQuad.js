016b88e646b1e7119e2cc2de8001d09f9a24733c
Avaer Kazmer <a@modules.io>
2018-04-30 06:54:28 -0400

    Small ScreenQuad cleanup

--- public/js/ScreenQuad.js
+++ public/js/ScreenQuad.js
@@ -6,8 +6,8 @@ window.ScreenQuad = (() => {
 
 		"varying vec2 vUv;",
 		"void main(){",
-			"vUv = 0.5 + (position.xy / 2.);",
-			"gl_Position = vec4( position.xy , 1. , 1. );",
+			"vUv = uv;",
+			"gl_Position = vec4(position.xy, 1., 1.);",
 		"}",
 
 	].join("\n");
