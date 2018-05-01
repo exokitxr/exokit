7e4dcfa6f4887c8328a7a1af19742cdd48fb3454
Shawn Presser <shawnpresser@gmail.com>
2018-05-01 08:17:40 -0500

    Simplify exokit.cmd

--- exokit.cmd
+++ exokit.cmd
@@ -1,13 +1,4 @@
 @echo off
-
 setlocal
-
-set home=%~dp0
-set node=%home%node\node.exe
-set code=%home%index.js
-
-IF EXIST "%node%" (
-  "%node%" "%code%" %*
-) ELSE (
-  node "%code%" %*
-)
+cd "%~dp0"
+node_modules\.bin\npx node . %*
