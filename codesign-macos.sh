57730600f416e67eca47c94e2adc5283edaba93e
Shawn Presser <shawnpresser@gmail.com>
2018-05-10 00:25:30 -0500

    Call codesign once per file under Exokit.app

--- codesign-macos.sh
+++ codesign-macos.sh
@@ -9,7 +9,7 @@ then
   security set-keychain-settings -t 3600 -u build.keychain
   security import codesign-macos.pfx -k build.keychain -P "${PFX_KEY}" -A
   security set-key-partition-list -S apple-tool:,apple: -s -k nopassword build.keychain
-  find /tmp/Exokit.app -type f | xargs codesign --force --verify --verbose --sign "7C22D41BA5AB743D3E47D543F6B27FE2FC720412"
+  find /tmp/Exokit.app -type f | xargs -n 1 codesign --force --verify --verbose --sign "7C22D41BA5AB743D3E47D543F6B27FE2FC720412"
   codesign --force --verify --verbose --sign "7C22D41BA5AB743D3E47D543F6B27FE2FC720412" /tmp/Exokit.app
   cp appdmg-codesign.json /tmp
   appdmg /tmp/appdmg-codesign.json exokit-macos-x64.dmg
