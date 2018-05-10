20a9ab0c5f10b5611747daa543b73a1f6467accd
Shawn Presser <shawnpresser@gmail.com>
2018-05-09 23:44:42 -0500

    Prevent signing errors when PFX_KEY is unset

--- codesign-macos.sh
+++ codesign-macos.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
 set -x
-find /tmp/Exokit.app -type f | xargs codesign --force --verify --verbose --sign "7C22D41BA5AB743D3E47D543F6B27FE2FC720412"
-codesign --force --verify --verbose --sign "7C22D41BA5AB743D3E47D543F6B27FE2FC720412" /tmp/Exokit.app
+
+if [ ! -z "${PFX_KEY}" ]
+then
+  security create-keychain -p nopassword build.keychain
+  security default-keychain -s build.keychain
+  security unlock-keychain -p nopassword build.keychain
+  security set-keychain-settings -t 3600 -u build.keychain
+  security import codesign-macos.pfx -k build.keychain -P "${PFX_KEY}" -A
+  security set-key-partition-list -S apple-tool:,apple: -s -k nopassword build.keychain
+  find /tmp/Exokit.app -type f | xargs codesign --force --verify --verbose --sign "7C22D41BA5AB743D3E47D543F6B27FE2FC720412"
+  codesign --force --verify --verbose --sign "7C22D41BA5AB743D3E47D543F6B27FE2FC720412" /tmp/Exokit.app
+  cp appdmg-codesign.json /tmp
+  appdmg /tmp/appdmg-codesign.json exokit-macos-x64.dmg
+else
+  cp appdmg.json /tmp
+  appdmg /tmp/appdmg.json exokit-macos-x64.dmg
+fi
