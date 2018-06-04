ec878697905b404d028cf104f695e702d2f61b97
Shawn Presser <shawnpresser@gmail.com>
2018-06-04 11:07:51 -0500

    Fix travis build for pull requests

---
+++ exokit-codesign-macos.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ -z "${PFX_KEY}" ]
+then
+  cp appdmg.json /tmp
+  appdmg /tmp/appdmg.json exokit-macos-x64.dmg
+else
+  security create-keychain -p nopassword build.keychain
+  security default-keychain -s build.keychain
+  security unlock-keychain -p nopassword build.keychain
+  security set-keychain-settings -t 3600 -u build.keychain
+  security import codesign-macos.pfx -k build.keychain -P "${PFX_KEY}" -A
+  security set-key-partition-list -S apple-tool:,apple: -s -k nopassword build.keychain
+  find /tmp/Exokit.app -type f | xargs -n 1 codesign --force --verify --verbose --sign "7C22D41BA5AB743D3E47D543F6B27FE2FC720412"
+  codesign --force --verify --verbose --sign "7C22D41BA5AB743D3E47D543F6B27FE2FC720412" /tmp/Exokit.app
+  cp appdmg-codesign.json /tmp
+  appdmg /tmp/appdmg-codesign.json exokit-macos-x64.dmg
+fi
