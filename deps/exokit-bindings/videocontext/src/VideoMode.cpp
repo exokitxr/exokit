abe44bc337c9438123f9b67e39c5bfd639daa194
Avaer Kazmer <a@modules.io>
2018-06-04 05:07:50 -0400

    Spacing cleanups

--- deps/exokit-bindings/videocontext/src/VideoMode.cpp
+++ deps/exokit-bindings/videocontext/src/VideoMode.cpp
@@ -173,17 +173,17 @@ VideoCamera::open(const char* deviceName, AVDictionary* options)
 
 template<typename Out>
 static void split(const std::string &s, char delim, Out result) {
-    std::stringstream ss(s);
-    std::string item;
-    while (std::getline(ss, item, delim)) {
-        *(result++) = item;
-    }
+  std::stringstream ss(s);
+  std::string item;
+  while (std::getline(ss, item, delim)) {
+    *(result++) = item;
+  }
 }
 
 static std::vector<std::string> split(const std::string &s, char delim) {
-    std::vector<std::string> elems;
-    split(s, delim, std::back_inserter(elems));
-    return elems;
+  std::vector<std::string> elems;
+  split(s, delim, std::back_inserter(elems));
+  return elems;
 }
 
 VideoCamera*
