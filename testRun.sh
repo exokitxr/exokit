b8e29f0c5afb52e969d4750573ccb027d341dc37
Chris Eddy <ChrisEddy@users.noreply.github.com>
2018-06-15 00:01:23 -0700

    New CI testRun script + CI intergration
    lets give this a shot, roast my code if it needs changes first

--- testRun.sh
+++ testRun.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-SCRIPT_PATH="./exokit.sh"
+"./exokit.sh" -t -h
 
-# Execute ./exokit.sh -h and log the console output to testRunLog.txt,
-# stdout and stderr included
-"$SCRIPT_PATH" -h >testRunLog.txt 2>&1 &
-echo "Exokit loaded, Exit Code: " $?
+exitCode=$?
 
-sleep 5s
-# Get process ID of exokit and kill it. (does not work )
-PID=$(pgrep -u root exokit.cmd)
-$(pkill -SIGKILL $PID)
-echo "Process killed, exit Code: " $?
-
-echo "Finished test run of ./exokit.sh -h"
-
-
-# keep console open for user to read or else goes away to quick
-read
+if [ $exitCode -ne 0 ]; then
+    echo "Error, Exokit failed to start! Exit code: " $exitCode
+else
+    echo "Success, Exokit started! Exit code: " $exitCode
+fi
+exit $exitCode
