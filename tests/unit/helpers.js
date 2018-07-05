b3962be3c33b1c621fe34bdcd8aacb4999fa1508
Kevin Ngo <me@ngokevin.com>
2018-07-05 16:44:21 -0700

    skip audio test on CI for now, errors on finding audio device

--- tests/unit/helpers.js
+++ tests/unit/helpers.js
@@ -1,5 +1,17 @@
+/* global describe, suite */
 const exokit = require('../../index');
 
 module.exports.createWindow = function () {
   return exokit().window;
-}
+};
+
+/**
+ * Test that is only run locally and is skipped on CI.
+ */
+module.exports.describeSkipCI = function () {
+  if (process.env.TEST_ENV === 'ci') {
+    return describe.skip;
+  } else {
+    return describe;
+  }
+};
