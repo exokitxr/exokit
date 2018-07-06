ee555762a3ec5fe273c1de5841bb29f98cfb9755
Avaer Kazmer <a@modules.io>
2018-07-06 03:40:48 -0400

    SrcableElement tests ordering cleanup in likelihood of crashing

--- tests/unit/dom/SrcableElement.test.js
+++ tests/unit/dom/SrcableElement.test.js
@@ -5,11 +5,11 @@ const path = require('path');
 const helpers = require('../helpers');
 const exokit = require('../../../index');
 
-const audioData = fs.readFileSync(path.resolve(__dirname, '../data/test.ogg'), 'base64');
-const audioUri = `data:audio/ogg;base64,${audioData}`;
-
 const imageData = fs.readFileSync(path.resolve(__dirname, '../data/test.png'), 'base64');
 const imageUri = `data:image/img;base64,${imageData}`;
+
+const audioData = fs.readFileSync(path.resolve(__dirname, '../data/test.ogg'), 'base64');
+const audioUri = `data:audio/ogg;base64,${audioData}`;
 
 const videoData = fs.readFileSync(path.resolve(__dirname, '../data/test.mp4'), 'base64');
 const videoUri = `data:video/mp4;base64,${videoData}`;
@@ -22,19 +22,19 @@ describe('HTMLSrcableElement', () => {
     document = exokit().document;
   });
 
-  helpers.describeSkipCI('<audio>', () => {
-    it('can set src', done => {
-      el = document.createElement('audio');
-      el.oncanplay = () => { done(); };
-      el.setAttribute('src', `${TEST_URL}/test.ogg`);
-    });
-  });
-
   describe('<img>', () => {
     it('can set src', done => {
       el = document.createElement('img');
       el.onload = () => { done(); };
       el.setAttribute('src', `${TEST_URL}/test.png`);
+    });
+  });
+
+  helpers.describeSkipCI('<audio>', () => {
+    it('can set src', done => {
+      el = document.createElement('audio');
+      el.oncanplay = () => { done(); };
+      el.setAttribute('src', `${TEST_URL}/test.ogg`);
     });
   });
 
