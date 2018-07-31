8934e904edc31dba0003649bb8579bf9e4a6af89
Avaer Kazmer <a@modules.io>
2018-07-30 22:47:40 -0400

    Add linux dependencies to exokit-install.sh script

--- exokit-install.sh
+++ exokit-install.sh
@@ -5,6 +5,10 @@
 
 echo "Exokit $VERSION install script"
 [ "$(whoami)" != 'root' ] && echo "This script must be run as root" && exit 1
+if [ -x "$(command -v apt-get)" ]; then
+  apt-get install -y \
+    build-essential wget python libglfw3-dev libglew-dev libfreetype6-dev libfontconfig1-dev uuid-dev libxcursor-dev libxinerama-dev libxi-dev libasound2-dev libexpat1-dev
+fi
 cd /tmp
 rm -Rf /usr/local/bin/exokit /usr/local/lib/exokit
 wget -O- https://get.webmr.io/linux-bin | tar -zxf - --directory /usr/local
