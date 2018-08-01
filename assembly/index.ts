0d9b1c5a51dd49c510177f72db1156689c5d4dd7
Avaer Kazmer <a@modules.io>
2018-08-01 06:47:11 -0400

    Udpate assembly example

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,5 +1,16 @@
 // The entry file of your WebAssembly module.
 
+declare namespace console {
+  function logi(val: i32): void;
+}
+
 export function add(a: i32, b: i32): i32 {
-  return a + b;
+  return a + b; //lol
+}
+
+export function main(): i32 {
+  console.logi(0);
+  // console.log('lol');
+  // prints('lol');
+  return 0;
 }
