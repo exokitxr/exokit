4892ec965c43a1eee344e2471915b3f380e27b9e
Avaer Kazmer <a@modules.io>
2018-08-08 07:52:01 -0400

    Add initial windows app package build materials

--- test.cmd
+++ test.cmd
@@ -4,10 +4,11 @@ mkdir out
 
 call ..\wabt\build\Debug\wasm2c build\optimized.wasm >out\main.c
 
-for %%I in (..\wabt\wasm2c\wasm-rt.h ..\wabt\wasm2c\wasm-rt-impl.h ..\wabt\wasm2c\wasm-rt-impl.c include\wasm.h) do copy %%I out
+for %%I in (..\wabt\wasm2c\wasm-rt.h ..\wabt\wasm2c\wasm-rt-impl.h ..\wabt\wasm2c\wasm-rt-impl.c include\wasm.h config\manifest.xml config\program.mabu config\app.package) do copy %%I out
 
 call node postprocess.js
 
-call cl /W4 out\main.c out\wasm-rt-impl.c /link /out:site.exe
-
-.\site.exe
+set MLSDK="C:\Users\avaer\MagicLeap\mlsdk\v0.16.0"
+echo calling "%MLSDK%\mabu.cmd"
+call %MLSDK%\mabu.cmd .\out\program.mabu
+call %MLSDK%\mabu.cmd -p --create-package --allow-unsigned .\out\app.package
