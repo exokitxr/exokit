f303c4e6563cbc329c19553aaa8089c6f87c3149
Avaer Kazmer <a@modules.io>
2018-08-08 05:20:48 -0400

    Update linux test.sh

--- test.sh
+++ test.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 npm run asbuild
+
 mkdir -p out
+
 ../wabt/bin/wasm2c build/optimized.wasm >out/main.c
+
 cp ../wabt/wasm2c/*.{h,c} out/
-sed -i -e 's/static u32 main(/u32 main(/g' out/main.c
+
+node postprocess.js
+
 cp include/wasm.h out/wasm.h
+
 gcc out/main.c out/wasm-rt-impl.c -o site
+
 ./site
