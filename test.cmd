23a0d0ab2da039f103f8f432d65cb194f74a266e
Avaer Kazmer <a@modules.io>
2018-08-09 16:25:02 -0400

    Clean up lumin target

--- test.cmd
+++ test.cmd
@@ -9,6 +9,6 @@ for %%I in (..\wabt\wasm2c\wasm-rt.h ..\wabt\wasm2c\wasm-rt-impl.h ..\wabt\wasm2
 call node postprocess.js
 
 set MLSDK="C:\Users\avaer\MagicLeap\mlsdk\v0.16.0"
-echo calling "%MLSDK%\mabu.cmd"
-call %MLSDK%\mabu.cmd .\out\program.mabu
-call %MLSDK%\mabu.cmd -p --create-package --allow-unsigned .\out\app.package
+
+call %MLSDK%\mabu.cmd -t debug_lumin .\out\program.mabu
+call %MLSDK%\mabu.cmd -t debug_lumin -s .\cert\app.privkey -p --create-package --allow-unsigned .\out\app.package
