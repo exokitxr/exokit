95fadd848ca9d4ebacd84380c27eb245d7b52c9c
Avaer Kazmer <a@modules.io>
2018-08-09 23:41:36 -0400

    Update build-ml script

--- build-ml.cmd
+++ build-ml.cmd
@@ -2,5 +2,8 @@ mkdir out
 
 set MLSDK="C:\Users\avaer\MagicLeap\mlsdk\v0.16.0"
 
-call %MLSDK%\mabu.cmd -t debug_lumin .\out\program.mabu
-call %MLSDK%\mabu.cmd -t debug_lumin -s .\cert\app.privkey -p --create-package --allow-unsigned .\out\app.package
+rem call %MLSDK%\mabu.cmd -v -t release_host .\program-windows.mabu
+rem call %MLSDK%\mabu.cmd -v -t debug_lumin -s .\cert\app.privkey -p --create-package --allow-unsigned .\app-windows.package
+
+call %MLSDK%\mabu.cmd -v -t debug_lumin .\program.mabu
+call %MLSDK%\mabu.cmd -v -t debug_lumin -s .\cert\app.privkey -p --create-package --allow-unsigned .\app.package
