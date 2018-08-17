1cd4af9d08cc07f20a3e4af15dfc36339c5b0030
Avaer Kazmer <a@modules.io>
2018-08-17 03:35:06 -0400

    Remove old build-ml.cmd script

--- build-ml.cmd
+++
@@ -1,9 +0,0 @@
-mkdir out
-
-set MLSDK="C:\Users\avaer\MagicLeap\mlsdk\v0.16.0"
-
-rem call %MLSDK%\mabu.cmd -v -t release_host .\program-windows.mabu
-rem call %MLSDK%\mabu.cmd -v -t release_host -m manifest-windows.xml -s .\cert\app.privkey -p --create-package --allow-unsigned .\app-windows.package
-
-call %MLSDK%\mabu.cmd -v -t debug_lumin .\program.mabu
-call %MLSDK%\mabu.cmd -v -t debug_lumin -s .\cert\app.privkey -p --create-package --allow-unsigned .\app.package
