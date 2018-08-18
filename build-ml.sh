619a238d6c513a0a1445e20c18ffca19b39bad7b
Avaer Kazmer <a@modules.io>
2018-08-18 06:24:57 -0400

    Clean up build-ml script

--- build-ml.sh
+++ build-ml.sh
@@ -18,16 +18,6 @@ EXTRA_FLAGS="-I/mnt/c/Users/avaer/MagicLeap/mlsdk/v0.16.0/lumin/stl/libc++/inclu
 export CFLAGS="$CFLAGS $EXTRA_FLAGS"
 export CXXFLAGS="$CXXFLAGS $EXTRA_FLAGS"
 
-# set AR=C:\Users\avaer\MagicLeap\mlsdk\v0.16.0\bin\aarch64-linux-android-ar.exe
-# set RANLIB=C:\Users\avaer\MagicLeap\mlsdk\v0.16.0\bin\aarch64-linux-android-ranlib.exe
-# set LINK=C:\Users\avaer\MagicLeap\mlsdk\v0.16.0\bin\aarch64-linux-android-ld.exe
-# set CPP=C:\Users\avaer\MagicLeap\mlsdk\v0.16.0\bin\aarch64-linux-android-g++.exe
-# set STRIP=C:\Users\avaer\MagicLeap\mlsdk\v0.16.0\bin\aarch64-linux-android-strip.exe
-# set OBJCOPY=C:\Users\avaer\MagicLeap\mlsdk\v0.16.0\bin\aarch64-linux-android-objcopy.exe
-# set LD=C:\Users\avaer\MagicLeap\mlsdk\v0.16.0\bin\aarch64-linux-android-ld.exe
-# set OBJDUMP=C:\Users\avaer\MagicLeap\mlsdk\v0.16.0\bin\aarch64-linux-android-objdump.exe
-# set NM=C:\Users\avaer\MagicLeap\mlsdk\v0.16.0\bin\aarch64-linux-android-nm.exe
-# set AS=C:\Users\avaer\MagicLeap\mlsdk\v0.16.0\bin\aarch64-linux-android-as.exe
 export npm_config_arch=arm64
 
 npm i --verbose --devdir="$(pwd)/.node-gyp" --arch=arm64 --target_arch=arm64 --LUMIN=true
