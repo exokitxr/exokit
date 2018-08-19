b91fc632b83204eedfe76b1beae745f7352ff936
Avaer Kazmer <a@modules.io>
2018-08-19 06:40:10 -0400

    Use magicleap-js/hack-toolchain.js in build-ml script

--- build-ml.sh
+++ build-ml.sh
@@ -2,11 +2,12 @@
 
 # cd /mnt/c/Users/avaer/Documents/GitHub/exokit
 
-rm -Rf node_modules
-npm cache clean --force
+# preface
 
 export MLSDK='/mnt/c/Users/avaer/MagicLeap/mlsdk/v0.16.0'
 export MLSDK_WIN='C:\Users\avaer\MagicLeap\mlsdk\v0.16.0'
+
+../magicleap-js/hack-toolchain.js
 
 export CC="$MLSDK/tools/toolchains/bin/aarch64-linux-android-clang"
 export CXX="$MLSDK/tools/toolchains/bin/aarch64-linux-android-clang++"
@@ -22,13 +23,21 @@ export LUMIN=1
 
 export npm_config_arch=arm64
 
+# npm install
+
+rm -Rf node_modules
+npm cache clean --force
 npm i --verbose --devdir="$(pwd)/.node-gyp" --arch=arm64 --target_arch=arm64
+
+# npm install libification
 
 rm -Rf mllib/libexokit.a
 find build/Release/obj.target node_modules -name '*.o' | xargs "$AR" crs mllib/libexokit.a
 ./gen-dlibs-h.js >dlibs.h
 
-# unhack
+# build mpk
+
+../magicleap-js/hack-toolchain.js -u
 
 rm -Rf build/magicleap
 cmd.exe /c "$MLSDK_WIN/mabu.cmd" "MLSDK=$MLSDK_WIN" -v -t release_lumin program-device.mabu
