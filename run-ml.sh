2ce43daee86e0c7b6000a7c083ce9184c3969bd0
avaer <a@modules.io>
2018-08-26 08:11:26 -0400

    Use exokit.mpk binplace for run-ml script

--- run-ml.sh
+++ run-ml.sh
@@ -3,7 +3,7 @@
 export MLSDK='/mnt/c/Users/avaer/MagicLeap/mlsdk/v0.16.0'
 export MLSDK_WIN='C:\Users\avaer\MagicLeap\mlsdk\v0.16.0'
 
-cmd.exe /c "$MLSDK_WIN/tools/debug/debug.py" --deploy-mpk build/magicleap/app-device/app-device.mpk build/magicleap/program-device/release_lumin_clang-3.8_aarch64/program-device
+cmd.exe /c "$MLSDK_WIN/tools/debug/debug.py" --deploy-mpk build/magicleap/exokit.mpk build/magicleap/program-device/release_lumin_clang-3.8_aarch64/program-device
 
 # %MLSDK%\tools\mldb\mldb.exe -d terminate -f com.webmr.exokit
 # %MLSDK%\tools\mldb\mldb.exe -d launch -f com.webmr.exokit
