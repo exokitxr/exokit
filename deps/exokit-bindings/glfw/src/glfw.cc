7c4bfe2a3ea1b68faf304374403e523a99065bb5
Michael Anthony <michael@activetheory.net>
2018-08-26 21:13:38 -0700

    Add support for gl_PointSize on desktop OpenGL

--- deps/exokit-bindings/glfw/src/glfw.cc
+++ deps/exokit-bindings/glfw/src/glfw.cc
@@ -1361,6 +1361,10 @@ NAN_METHOD(Create) {
       glGenVertexArrays(1, &vao);
       glBindVertexArray(vao);
 
+      #ifdef GL_VERTEX_PROGRAM_POINT_SIZE
+      glEnable(GL_VERTEX_PROGRAM_POINT_SIZE);
+      #endif
+
       Local<Array> result = Nan::New<Array>(8);
       result->Set(0, pointerToArray(windowHandle));
       result->Set(1, JS_INT(framebuffers[0]));
