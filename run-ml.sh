4c26104544cd4ddcd46a5417f8e02dafb3f20847
avaer <a@modules.io>
2018-08-30 00:56:06 -0400

    Add run-ml script

---
+++ run-ml.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export MLSDK=${MLSDK:-/mnt/c/Users/avaer/MagicLeap/mlsdk/v0.16.0}
+export MLSDK_WIN=$(echo "$MLSDK" | sed 's/^\/mnt\/c\//C:\\/' | sed 's/\//\\/g')
+
+cmd.exe /c "$MLSDK_WIN/tools/mldb/mldb.exe" terminate -f com.webmr.exokit
+cmd.exe /c "$MLSDK_WIN/tools/mldb/mldb.exe" launch com.webmr.exokit -v "ARGS=node . $@"
+cmd.exe /c "$MLSDK_WIN/tools/mldb/mldb.exe" log exokit:*
