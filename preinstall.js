f9dc00f820838a8642fbf2e132e9d65bb505d610
Avaer Kazmer <a@modules.io>
2018-09-04 19:47:59 -0400

    Remove magicleap module build dependency from preinstall.js

--- preinstall.js
+++ preinstall.js
@@ -4,7 +4,7 @@ const childProcess = require('child_process');
 
 const packageJson = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json')));
 const dependencies = Object.keys(packageJson.dependencies);
-const nativeDependencies = [...dependencies.filter(dependency => /^native-/.test(dependency)), 'magicleap']
+const nativeDependencies = [...dependencies.filter(dependency => /^native-/.test(dependency))]
 
 for (let i = 0; i < nativeDependencies.length; i++) {
   childProcess.execSync('npm run postinstall .', {
