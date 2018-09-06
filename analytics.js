9019ed34c91959768e3b592708ce0f8f229e0559
Avaer Kazmer <a@modules.io>
2018-09-06 16:52:25 -0400

    Refactor analytics

--- analytics.js
+++ analytics.js
@@ -6,12 +6,13 @@ const os = require('os');
 
 const packageJson = require('./package.json');
 const bugsnag = require('bugsnag');
+const Mixpanel = require('mixpanel');
 
 const GlobalContext = require('./src/GlobalContext');
 
-const bugsnagApiKey = (() => {
+const analyticsJson = (() => {
   try {
-    return fs.readFileSync(path.join(__dirname, 'bugsnag.txt'), 'utf8').match(/^(\S*)/)[1];
+    return JSON.parse(fs.readFileSync(path.join(__dirname, 'analytics.json'), 'utf8'));
   } catch (err) {
     if (err.code !== 'ENOENT') {
       console.warn(err.stack);
@@ -19,11 +20,12 @@ const bugsnagApiKey = (() => {
     return null;
   }
 })();
+const bugsnagApiKey = analyticsJson && analyticsJson.bugsnag;
 if (bugsnagApiKey) {
   bugsnag.register(bugsnagApiKey, {
     metaData: {
       argv: process.argv,
-      command: GlobalContext.commands,
+      command: GlobalContext.commands || [],
       packageJson,
       arch: os.arch(),
       cpus: os.cpus(),
@@ -42,6 +44,11 @@ if (bugsnagApiKey) {
       userInfo: os.userInfo(),
     },
   });
+}
+
+const mixpanelApiKey = analyticsJson && analyticsJson.mixpanel;
+if (mixpanelApiKey) {
+  GlobalContext.mixpanel = Mixpanel.init(mixpanelApiKey);
 }
 
 if (require.main === module) {
