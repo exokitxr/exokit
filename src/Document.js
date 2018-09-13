f1acafcfe13088630bc530abb15c04b683fe6047
avaer <a@modules.io>
2018-09-12 21:05:12 -0400

    Use setImmediate instead of process.nextTick for firing DOMContentLoaded

--- src/Document.js
+++ src/Document.js
@@ -147,7 +147,7 @@ function initDocument (document, window) {
     });
   }
 
-  process.nextTick(async () => {
+  setImmediate(async () => {
     if (body) {
       const bodyChildNodes = body.childNodes;
       body.childNodes = new DOM.NodeList();
