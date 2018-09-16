f0d7dbc9c8f8777f4c7ce73380bd2481784183a3
Kevin Ngo <me@ngokevin.com>
2018-09-16 05:40:24 -0700

    bind EXT_blend_minmax webgl extension

---
+++ tests/unit/webgl.test.js
@@ -0,0 +1,24 @@
+/* global afterEach, beforeEach, describe, assert, it */
+const exokit = require('../../src/index');
+
+describe.only('webgl', () => {
+  var gl;
+  var window;
+
+  beforeEach(() => {
+    window = exokit().window;
+    gl = window.WebGLRenderingContext(window.document.createElement('canvas'));
+  });
+
+  afterEach(() => {
+    window.destroy();
+  });
+
+  describe('getExtension', () => {
+    it('enables EXT_blend_minmax', () => {
+      const minmax = gl.getExtension('EXT_blend_minmax');
+      assert.equal(typeof minmax.MIN_EXT, 'number');
+      assert.equal(typeof minmax.MAX_EXT, 'number');
+    });
+  });
+});
