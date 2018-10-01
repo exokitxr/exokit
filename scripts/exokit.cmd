f1647baaa778fb14ceb8420bd643545319f02818
Avaer Kazmer <a@modules.io>
2018-09-30 20:01:25 -0400

    Bugfix windows start batch scripts

--- scripts/exokit.cmd
+++ scripts/exokit.cmd
@@ -3,8 +3,8 @@
 setlocal
 
 set home=%~dp0
-set node=%home%node\node.exe
-set code=%home%index.js
+set node=%home%..\node\node.exe
+set code=%home%..\src\index.js
 
 IF EXIST "%node%" (
   "%node%" "%code%" %*
