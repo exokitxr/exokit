2fd5cffc6f0dc3be8cb3bf4905c4d7b9958d3b73
machenmusik <machenmusik@users.noreply.github.com>
2018-10-14 15:59:09 -0400

    Update debug-ml.sh

--- scripts/debug-ml.sh
+++ scripts/debug-ml.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export MLSDK='/mnt/c/Users/avaer/MagicLeap/mlsdk/v0.16.0'
-export MLSDK_WIN='C:\Users\avaer\MagicLeap\mlsdk\v0.16.0'
+export MLSDK=${MLSDK:-/mnt/c/Users/avaer/MagicLeap/mlsdk/v0.16.0}
+export MLSDK_WIN=$(echo "$MLSDK" | sed 's/^\/mnt\/\([a-z]\)\//\1:\\/' | sed 's/\//\\/g')
 
 cmd.exe /c "$MLSDK_WIN/tools/debug/debug.py" --deploy-mpk build/magicleap/exokit.mpk build/magicleap/program-device/release_lumin_clang-3.8_aarch64/program-device --env "ARGS=node . $@"
