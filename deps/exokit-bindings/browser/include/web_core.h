f91ef03b0f8ace5cee6fbf1541344657bb50df10
avaer <a@modules.io>
2018-10-29 09:42:27 -0400

    Hook in initial browser bindings

--- deps/exokit-bindings/browser/include/web_core.h
+++ deps/exokit-bindings/browser/include/web_core.h
@@ -1,14 +1,22 @@
-﻿// Ŭnicode please
+﻿#ifndef _WEB_CORE_H_
+#define _WEB_CORE_H_
+
 #include <include/cef_client.h>
+#include <include/cef_app.h>
+#include <include/wrapper/cef_helpers.h>
+
+#include "render_handler.h"
+
 #include <string>
 #include <vector>
+#include <functional>
 
 class RenderHandler;
 class BrowserClient;
 
 class WebCore {
 public:
-	WebCore(const std::string &url);
+	WebCore(const std::string &url, RenderHandler::OnPaintFn onPaint);
 	~WebCore();
 
 	void reshape(int w, int h);
@@ -39,9 +47,30 @@ public:
 
 	void update();
 
-	std::weak_ptr<WebCore> createBrowser(const std::string &url);
+	std::weak_ptr<WebCore> createBrowser(const std::string &url, RenderHandler::OnPaintFn onPaint);
 	void removeBrowser(std::weak_ptr<WebCore> web_core);
 
 private:
 	std::vector<std::shared_ptr<WebCore>> browsers_;
 };
+
+class SimpleApp : public CefApp, public CefBrowserProcessHandler {
+ public:
+  SimpleApp();
+
+  // CefApp methods:
+  virtual CefRefPtr<CefBrowserProcessHandler> GetBrowserProcessHandler() override {
+    return this;
+  }
+
+  virtual void OnBeforeCommandLineProcessing(const CefString &process_type, CefRefPtr<CefCommandLine> command_line) override;
+  
+  // CefBrowserProcessHandler methods:
+  virtual void OnContextInitialized() override;
+
+ private:
+  // Include the default reference counting implementation.
+  IMPLEMENT_REFCOUNTING(SimpleApp);
+};
+
+#endif
--- deps/exokit-bindings/browser/src/render_handler.cpp
+++ deps/exokit-bindings/browser/src/render_handler.cpp
@@ -1,14 +1,13 @@
-﻿// Ŭnicode please
-#include "render_handler.h"
+﻿#include "render_handler.h"
 
-RenderHandler::RenderHandler()
-	: width_(2), height_(2), tex_(0) 
-{
-}
+#include <iostream>
+
+RenderHandler::RenderHandler(OnPaintFn onPaint) : onPaint(onPaint), width_(2), height_(2)/*, tex_(0)*/ {}
 
 void RenderHandler::init()
 {
-	glGenTextures(1, &tex_);
+  std::cout << "RenderHandler::init" << std::endl;
+	/* glGenTextures(1, &tex_);
 	glBindTexture(GL_TEXTURE_2D, tex_);
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
@@ -25,23 +24,49 @@ void RenderHandler::init()
 	};
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 2, 2, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
 
-	glBindTexture(GL_TEXTURE_2D, 0);
+	glBindTexture(GL_TEXTURE_2D, 0); */
 }
 void RenderHandler::resize(int w, int h)
 {
+  // std::cout << "RenderHandler::resize" << std::endl;
 	width_ = w;
 	height_ = h;
 }
 
 bool RenderHandler::GetViewRect(CefRefPtr<CefBrowser> browser, CefRect &rect)
 {
+  // std::cout << "RenderHandler::GetViewRect" << std::endl;
 	rect = CefRect(0, 0, width_, height_);
 	return true;
 }
 
 void RenderHandler::OnPaint(CefRefPtr<CefBrowser> browser, PaintElementType type, const RectList &dirtyRects, const void *buffer, int width, int height)
 {
-	glBindTexture(GL_TEXTURE_2D, tex_);
+  // std::cout << "RenderHandler::OnPaint" << std::endl;
+  onPaint(dirtyRects, buffer, width, height);
+  
+	/* glBindTexture(GL_TEXTURE_2D, tex_);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_BGRA_EXT, GL_UNSIGNED_BYTE, (unsigned char*)buffer);
-	glBindTexture(GL_TEXTURE_2D, 0);
+	glBindTexture(GL_TEXTURE_2D, 0); */
+  
+  /* glPixelStorei(GL_UNPACK_ROW_LENGTH, view_width_);
+
+  if (old_width != view_width_ || old_height != view_height_) {
+    // Update/resize the whole texture.
+    glPixelStorei(GL_UNPACK_SKIP_PIXELS, 0);
+    glPixelStorei(GL_UNPACK_SKIP_ROWS, 0);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, view_width_, view_height_, 0,
+                 GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, buffer);
+  } else {
+    // Update just the dirty rectangles.
+    CefRenderHandler::RectList::const_iterator i = dirtyRects.begin();
+    for (; i != dirtyRects.end(); ++i) {
+      const CefRect& rect = *i;
+      glPixelStorei(GL_UNPACK_SKIP_PIXELS, rect.x);
+      glPixelStorei(GL_UNPACK_SKIP_ROWS, rect.y);
+      glTexSubImage2D(GL_TEXTURE_2D, 0, rect.x, rect.y, rect.width,
+                      rect.height, GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV,
+                      buffer);
+    }
+  } */
 }
