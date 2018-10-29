86188f014cbf1d80085ea48931dc537f73f7bd0b
Avaer Kazmer <a@modules.io>
2018-10-29 09:36:49 -0400

    Add initial cef-gl-example cef browser binding source

---
+++ deps/exokit-bindings/browser/src/render_handler.cpp
@@ -0,0 +1,47 @@
+﻿// Ŭnicode please
+#include "render_handler.h"
+
+RenderHandler::RenderHandler()
+	: width_(2), height_(2), tex_(0) 
+{
+}
+
+void RenderHandler::init()
+{
+	glGenTextures(1, &tex_);
+	glBindTexture(GL_TEXTURE_2D, tex_);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+	// dummy texture data - for debugging
+	const unsigned char data[] = {
+		255, 0, 0, 255,
+		0, 255, 0, 255,
+		0, 0, 255, 255,
+		255, 255, 255, 255,
+	};
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 2, 2, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+}
+void RenderHandler::resize(int w, int h)
+{
+	width_ = w;
+	height_ = h;
+}
+
+bool RenderHandler::GetViewRect(CefRefPtr<CefBrowser> browser, CefRect &rect)
+{
+	rect = CefRect(0, 0, width_, height_);
+	return true;
+}
+
+void RenderHandler::OnPaint(CefRefPtr<CefBrowser> browser, PaintElementType type, const RectList &dirtyRects, const void *buffer, int width, int height)
+{
+	glBindTexture(GL_TEXTURE_2D, tex_);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_BGRA_EXT, GL_UNSIGNED_BYTE, (unsigned char*)buffer);
+	glBindTexture(GL_TEXTURE_2D, 0);
+}
