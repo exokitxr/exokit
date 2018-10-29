15232390484031ba4924a4cf2a56683e344a5d06
Avaer Kazmer <a@modules.io>
2018-10-29 09:36:49 -0400

    Add initial cef-gl-example cef browser binding source

---
+++ deps/exokit-bindings/browser/src/browser_client.cpp
@@ -0,0 +1,8 @@
+﻿// Ŭnicode please
+#include "browser_client.h"
+#include "render_handler.h"
+
+BrowserClient::BrowserClient(RenderHandler *renderHandler)
+	: m_renderHandler(renderHandler)
+{
+}
---
+++ deps/exokit-bindings/browser/src/render_handler.h
@@ -0,0 +1,31 @@
+﻿// Ŭnicode please
+#include <GL/glew.h>
+#include <include/cef_render_handler.h>
+
+class RenderHandler : public CefRenderHandler
+{
+public:
+	RenderHandler();
+
+public:
+	void init();
+	void resize(int w, int h);
+
+	// CefRenderHandler interface
+public:
+	bool GetViewRect(CefRefPtr<CefBrowser> browser, CefRect &rect);
+	void OnPaint(CefRefPtr<CefBrowser> browser, PaintElementType type, const RectList &dirtyRects, const void *buffer, int width, int height);
+
+	// CefBase interface
+public:
+	IMPLEMENT_REFCOUNTING(RenderHandler);
+
+public:
+	GLuint tex() const { return tex_; }
+
+private:
+	int width_;
+	int height_;
+
+	GLuint tex_;
+};
