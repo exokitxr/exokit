eea5bb76a6ef14126949ed89d2d4b662caada40c
avaer <a@modules.io>
2018-10-29 21:32:44 -0400

    Integrate browser rendering code into single browser.{h,cpp}

--- deps/exokit-bindings/browser/include/Browser.h
+++ deps/exokit-bindings/browser/include/Browser.h
@@ -2,7 +2,6 @@
 #define _BROWSER_H_
 
 #include <webgl.h>
-#include "web_core.h"
 
 #include <v8.h>
 #include <node.h>
@@ -10,13 +9,85 @@
 
 #include <defines.h>
 
+#include <functional>
+
+#include <include/cef_client.h>
+#include <include/cef_app.h>
+#include <include/cef_render_handler.h>
+#include <include/wrapper/cef_helpers.h>
+
 using namespace std;
 using namespace v8;
 using namespace node;
 
 namespace browser {
+
+// SimpleApp
+
+class SimpleApp : public CefApp, public CefBrowserProcessHandler {
+public:
+  SimpleApp();
+
+  // CefApp methods:
+  virtual CefRefPtr<CefBrowserProcessHandler> GetBrowserProcessHandler() override {
+    return this;
+  }
+
+  virtual void OnBeforeCommandLineProcessing(const CefString &process_type, CefRefPtr<CefCommandLine> command_line) override;
   
-extern bool cefInitialized;
+  // CefBrowserProcessHandler methods:
+  virtual void OnContextInitialized() override;
+
+private:
+  // Include the default reference counting implementation.
+  IMPLEMENT_REFCOUNTING(SimpleApp);
+};
+
+// RenderHandler
+
+class RenderHandler : public CefRenderHandler {
+public:
+  typedef std::function<void(const RectList &, const void *, int, int)> OnPaintFn;
+  
+public:
+	RenderHandler(OnPaintFn onPaint);
+  ~RenderHandler();
+
+	void resize(int w, int h);
+
+	// CefRenderHandler interface
+public:
+	bool GetViewRect(CefRefPtr<CefBrowser> browser, CefRect &rect);
+	void OnPaint(CefRefPtr<CefBrowser> browser, PaintElementType type, const RectList &dirtyRects, const void *buffer, int width, int height);
+
+	// CefBase interface
+private:
+  IMPLEMENT_REFCOUNTING(RenderHandler);
+
+private:
+  int width;
+	int height;
+  std::function<void(const RectList &, const void *, int, int)> onPaint;
+};
+
+// BrowserClient
+
+class BrowserClient : public CefClient {
+public:
+	BrowserClient(RenderHandler *renderHandler);
+  ~BrowserClient();
+
+	virtual CefRefPtr<CefRenderHandler> GetRenderHandler() {
+		return m_renderHandler;
+	}
+
+	CefRefPtr<CefRenderHandler> m_renderHandler;
+
+private:
+	IMPLEMENT_REFCOUNTING(BrowserClient);
+};
+
+// Browser
 
 class Browser : public ObjectWrap {
 public:
@@ -26,6 +97,7 @@ public:
 protected:
   static NAN_METHOD(New);
   static NAN_METHOD(Update);
+  void reshape(int w, int h);
 
   Browser(WebGLRenderingContext *gl, GLuint tex, int width, int height, const std::string &url);
   ~Browser();
@@ -33,8 +105,13 @@ protected:
 protected:
   GLuint tex;
   bool initialized;
-  std::unique_ptr<WebCore> web_core;
+  // std::unique_ptr<WebCore> web_core;
+  std::unique_ptr<RenderHandler> render_handler_;
+  std::unique_ptr<BrowserClient> client_;
+  CefRefPtr<CefBrowser> browser_;
 };
+
+extern bool cefInitialized;
 
 }
 
