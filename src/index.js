3f024ba4c676dad54061f4a7f2950797e5e2fe8f
avaer <a@modules.io>
2018-11-01 12:07:30 -0400

    Small index.js commenting cleanup

--- src/index.js
+++ src/index.js
@@ -877,7 +877,7 @@ const _bindWindow = (window, newWindowCb) => {
   window.on('navigate', newWindowCb);
   window.document.on('paste', e => {
     e.clipboardData = new window.DataTransfer();
-    if (contexts.length > 0) {
+    if (contexts.length > 0) { // XXX use the currently focused context
       const context = contexts[0];
       const windowHandle = context.getWindowHandle();
       const clipboardContents = nativeWindow.getClipboard(windowHandle).slice(0, 256);
