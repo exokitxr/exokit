e2fa2924497af34bf8e2a2f1b218f372d3f19d7c
avaer <a@modules.io>
2018-11-04 13:19:49 -0500

    Debug listeners on* attachment handling

--- src/utils.js
+++ src/utils.js
@@ -153,11 +153,13 @@ module.exports._elementGetter = _elementGetter;
 const _elementSetter = (self, attribute, cb) => {
   const listener = _elementGetter(self, attribute);
   if (listener) {
-    self.removeEventListener(attriubute, listener);
+    self.removeEventListener(attribute, listener);
+    listener[symbols.listenerSymbol] = false;
   }
   
   if (typeof cb === 'function') {
     self.addEventListener(attribute, cb);
+    cb[symbols.listenerSymbol] = true;
   }
 };
 module.exports._elementSetter = _elementSetter;
