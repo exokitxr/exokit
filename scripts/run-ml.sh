99575ee6f0bdfb8cfed10d9923508da759bb3577
avaer <a@modules.io>
2018-11-16 15:16:07 -0500

    Update build scripts MLSDK versions to 0.18.0

--- scripts/run-ml.sh
+++ scripts/run-ml.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export MLSDK=${MLSDK:-/mnt/c/Users/avaer/MagicLeap/mlsdk/v0.16.0}
+export MLSDK=${MLSDK:-/mnt/c/Users/avaer/MagicLeap/mlsdk/v0.18.0}
 export MLSDK_WIN=$(echo "$MLSDK" | sed 's/^\/mnt\/\([a-z]\)\//\1:\\/' | sed 's/\//\\/g')
 
 cmd.exe /c "$MLSDK_WIN/tools/mldb/mldb.exe" terminate -f com.webmr.exokit
