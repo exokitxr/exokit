387876d0133ee79d8e9ba1d80159f11fe78bcd64
avaer <a@modules.io>
2018-11-23 22:46:22 -0500

    Use full floor vector again

--- deps/exokit-bindings/magicleap/src/magicleap.cc
+++ deps/exokit-bindings/magicleap/src/magicleap.cc
@@ -87,10 +87,13 @@ MLPlane floorResults[MAX_NUM_PLANES];
 uint32_t numFloorResults;
 bool floorRequestPending = false;
 struct LargestFloor {
-  float y;
+  MLVec3f position;
   float size;
 };
-LargestFloor largestFloor{0, 0};
+LargestFloor largestFloor{
+  {0, 0, 0}
+  0,
+};
 
 MLHandle planesTracker;
 std::vector<MLPlaneTracker *> planeTrackers;
@@ -2709,7 +2712,7 @@ NAN_METHOD(MLContext::PostPollEvents) {
 
         if (size > largestFloor.size) {
           largestFloor = {
-            plane.position.y,
+            plane.position,
             size,
           };
         }
