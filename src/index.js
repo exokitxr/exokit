3e7ca73e79d64bf7d21d63b9bcae89dc96559337
Shawn Presser <shawnpresser@gmail.com>
2018-11-28 15:10:18 -0600

    Get rid of unused symbols import in index.js

--- src/index.js
+++ src/index.js
@@ -21,7 +21,6 @@ const UPNG = require('upng-js');
 
 const {version} = require('../package.json');
 const nativeBindingsModulePath = path.join(__dirname, 'native-bindings.js');
-const symbols = require('./symbols');
 const {THREE} = core;
 const nativeBindings = require(nativeBindingsModulePath);
 const {nativeVideo, nativeVr, nativeLm, nativeMl, nativeWindow, nativeAnalytics} = nativeBindings;
