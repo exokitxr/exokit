3f51dfaba66afe15db3228cdfbf74aeecbaeec98
Avaer Kazmer <a@modules.io>
2018-12-07 13:15:54 -0500

    Major WindowVm tickAnimationFrame work

--- src/WindowVm.js
+++ src/WindowVm.js
@@ -6,96 +6,67 @@ const vmOne = require('vm-one');
 const GlobalContext = require('./GlobalContext');
 const symbols = require('./symbols');
 
-let rafCbs = [];
-let timeouts = [];
-let intervals = [];
-let rafIndex = 0;
-const localCbs = [];
-const _cacheLocalCbs = cbs => {
-  for (let i = 0; i < cbs.length; i++) {
-    localCbs[i] = cbs[i];
-  }
-  for (let i = cbs.length; i < localCbs.length; i++) {
-    localCbs[i] = null;
-  }
+const windowVms = [];
+const _makeWindowVm = (htmlString = '', options = {}) => {
+  const v = vmOne.make({
+    initModule: path.join(__dirname, 'Window.js'),
+    args: {
+      htmlString,
+      options,
+      args: GlobalContext.args,
+    },
+  });
+  v.hidden = !!options.hidden;
+  v.tickAnimationFrame = timestamp => {
+    v.postMessage({
+      method: 'tickAnimationFrame',
+      timestamp,
+    });
+  };
+  v.updateXrFrame = update => {
+    v.postMessage({
+      method: 'updateXrFrame',
+      update,
+    });
+  };
+
+  windowVms.push(v);
+  v.on('exit', () => {
+    windowVms.splice(windowVms.indexOf(v), 1);
+  });
+
+  return v;
 };
-const _clearLocalCbs = () => {
-  for (let i = 0; i < localCbs.length; i++) {
-    localCbs[i] = null;
-  }
-};
+module.exports._makeWindowVm = _makeWindowVm;
+
 function tickAnimationFrame() {
-  if (rafCbs.length > 0) {
-    _cacheLocalCbs(rafCbs);
-
-    tickAnimationFrame.window = this;
-
+  if (windowVms.length > 0) {
     const performanceNow = performance.now();
 
-    // hidden rafs
-    for (let i = 0; i < localCbs.length; i++) {
-      const rafCb = localCbs[i];
-      if (rafCb && rafCb[symbols.windowSymbol].document.hidden) {
-        try {
-          rafCb(performanceNow);
-        } catch (e) {
-          console.warn(e);
-        }
+    // hidden
+    for (let i = 0; i < windowVms.length; i++) {
+      const windowVm = windowVms[i];
 
-        const index = rafCbs.indexOf(rafCb); // could have changed due to sorting
-        if (index !== -1) {
-          rafCbs[index] = null;
-        }
-      }
-    }
-    // visible rafs
-    for (let i = 0; i < localCbs.length; i++) {
-      const rafCb = localCbs[i];
-      if (rafCb && !rafCb[symbols.windowSymbol].document.hidden) {
-        try {
-          rafCb(performanceNow);
-        } catch (e) {
-          console.warn(e);
-        }
-        const index = rafCbs.indexOf(rafCb); // could have changed due to sorting
-        if (index !== -1) {
-          rafCbs[index] = null;
-        }
+      if (windowVm.hidden) {
+        tickAnimationFrame.windowVm = windowVm;
+
+        windowVm.tickAnimationFrame(performanceNow);
       }
     }
 
-    tickAnimationFrame.window = null;
+    // visible
+    for (let i = 0; i < windowVms.length; i++) {
+      const windowVm = windowVms[i];
+
+      if (!windowVm.hidden) {
+        tickAnimationFrame.windowVm = windowVm;
+
+        windowVm.tickAnimationFrame(performanceNow);
+      }
+    }
+
+    tickAnimationFrame.windowVm = null;
   }
-
-  _clearLocalCbs(); // release garbage
 }
-tickAnimationFrame.window = null;
-const _findFreeSlot = a => {
-  let i;
-  for (i = 0; i < a.length; i++) {
-    if (a[i] === null) {
-      break;
-    }
-  }
-  return i;
-};
-const _makeRequestAnimationFrame = window => (fn, priority = 0) => {
-  fn = fn.bind(window);
-  fn[symbols.windowSymbol] = window;
-  fn[symbols.prioritySymbol] = priority;
-  const id = ++rafIndex;
-  fn[symbols.idSymbol] = id;
-  rafCbs[_findFreeSlot(rafCbs)] = fn;
-  rafCbs.sort((a, b) => (b ? b[symbols.prioritySymbol] : 0) - (a ? a[symbols.prioritySymbol] : 0));
-  return id;
-};
-
-const _makeWindowVm = (htmlString = '', options = {}) => vmOne.make({
-  initModule: path.join(__dirname, 'Window.js'),
-  args: {
-    htmlString,
-    options,
-    args: GlobalContext.args,
-  },
-});
-module.exports._makeWindowVm = _makeWindowVm;
+tickAnimationFrame.windowVm = null;
+module.exports.tickAnimationFrame = tickAnimationFrame
