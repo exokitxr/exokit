246b7af63b7dcb6446273707454aff6c95dd74b0
Avaer Kazmer <a@modules.io>
2018-12-08 20:46:57 -0500

    Syntax bugfixing

--- src/index.js
+++ src/index.js
@@ -134,7 +134,7 @@ const _windowHandleEquals = (a, b) => a[0] === b[0] && a[1] === b[1];
 
 let _takeScreenshot = false;
 
-nativeBindings.nativeGl && nativeBindings.nativeGl.onconstruct = (gl, canvas) => {
+nativeBindings.nativeGl && (nativeBindings.nativeGl.onconstruct = (gl, canvas) => {
   const canvasWidth = canvas.width || innerWidth;
   const canvasHeight = canvas.height || innerHeight;
 
@@ -273,9 +273,9 @@ nativeBindings.nativeGl && nativeBindings.nativeGl.onconstruct = (gl, canvas) =>
   } else {
     return false;
   }
-};
+});
 
-nativeBindings.nativeCanvasRenderingContext2D && nativeBindings.nativeCanvasRenderingContext2D.onconstruct = (ctx, canvas) => {
+nativeBindings.nativeCanvasRenderingContext2D && (nativeBindings.nativeCanvasRenderingContext2D.onconstruct = (ctx, canvas) => {
   const canvasWidth = canvas.width || innerWidth;
   const canvasHeight = canvas.height || innerHeight;
 
@@ -313,7 +313,7 @@ nativeBindings.nativeCanvasRenderingContext2D && nativeBindings.nativeCanvasRend
   } else {
     return false;
   }
-};
+});
 
 const zeroMatrix = new THREE.Matrix4();
 const localFloat32Array = zeroMatrix.toArray(new Float32Array(16));
