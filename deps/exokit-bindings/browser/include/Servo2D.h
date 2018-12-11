743473563f2f8558339b448d9db47992e991f486
avaer <a@modules.io>
2018-12-11 17:32:57 -0500

    Major Servo2D bindings cleanup

--- deps/exokit-bindings/browser/include/Servo2D.h
+++ deps/exokit-bindings/browser/include/Servo2D.h
@@ -2,30 +2,33 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include <GLES/gl.h>
-#include <lumin/LandscapeApp.h>
-#include <lumin/Prism.h>
-#include <lumin/event/ServerEvent.h>
-#include <lumin/event/GestureInputEventData.h>
-#include <lumin/event/KeyInputEventData.h>
-#include <lumin/event/ControlTouchPadInputEventData.h>
-#include <lumin/event/ControlPose6DofInputEventData.h>
-#include <lumin/node/LineNode.h>
-#include <lumin/node/QuadNode.h>
-#include <lumin/resource/PlanarResource.h>
-#include <lumin/ui/KeyboardDefines.h>
-#include <lumin/ui/node/UiButton.h>
-#include <lumin/ui/node/UiPanel.h>
-#include <lumin/ui/node/UiTextEdit.h>
-#include <SceneDescriptor.h>
+#include <webgl.h>
+#include <ml_logging.h>
+// #include <string.h>
 
+// forward declarations
+
+class Servo2D;
 typedef struct Opaque ServoInstance;
+typedef void (*MLLogger)(MLLogLevel lvl, char* msg);
+typedef void (*MLHistoryUpdate)(Servo2D* app, bool canGoBack, char* url, bool canGoForward);
+
+extern "C" ServoInstance* init_servo(EGLContext, EGLSurface, EGLDisplay,
+                                     Servo2D*, MLLogger, MLHistoryUpdate,
+                                     const char* url, int width, int height, float hidpi);
+extern "C" void heartbeat_servo(ServoInstance*);
+extern "C" void trigger_servo(ServoInstance*, float x, float y, bool down);
+extern "C" void move_servo(ServoInstance*, float x, float y);
+extern "C" void traverse_servo(ServoInstance*, int delta);
+extern "C" void navigate_servo(ServoInstance*, const char* text);
+extern "C" void discard_servo(ServoInstance*);
 
 /**
  * Servo2D Landscape Application
  */
-class Servo2D : public lumin::LandscapeApp {
+class Servo2D {
 public:
+
   /**
    * Constructs the Landscape Application.
    */
@@ -34,7 +37,7 @@ public:
   /**
    * Destroys the Landscape Application.
    */
-  virtual ~Servo2D();
+  ~Servo2D();
 
   /**
    * Disallows the copy constructor.
@@ -56,81 +59,50 @@ public:
    */
   Servo2D& operator=(Servo2D&&) = delete;
 
-  /**
-   * Update the browser history UI
-   */
-  void updateHistory(bool canGoBack, const char* url, bool canGoForward);
-
-protected:
+// protected:
   /**
    * Initializes the Landscape Application.
    * @return - 0 on success, error code on failure.
    */
-  int init() override;
+  int init(
+    const std::string &url,
+    NATIVEwindow *window,
+    GLuint tex,
+    int width,
+    int height,
+    std::function<void()> onloadstart,
+    std::function<void(const std::string &)> onloadend,
+    std::function<void(int, const std::string &, const std::string &)> onloaderror,
+    std::function<void(const std::string &, const std::string &, int)> onconsole,
+    std::function<void(const std::string &)> onmessage
+  );
 
   /**
    * Deinitializes the Landscape Application.
    * @return - 0 on success, error code on failure.
    */
-  int deInit() override;
-
-  /**
-   * Returns the size of the Prism, default = +/- (1.0f, 1.0f, 1.0f) meters.
-   * Used in createPrism().
-   */
-  const glm::vec3 getInitialPrismExtents() const;
-
-  /**
-   * Creates the prism, updates the private variable prism_ with the created prism.
-   */
-  int createInitialPrism();
-
-  /**
-   * Initializes and creates the scene of all scenes marked as initially instanced
-   */
-  void instanceInitialScenes();
-
-  /**
-   * Initializes and creates the scene of the scene and instances it into the prism
-   */
-  lumin::Node* instanceScene(const SceneDescriptor & sceneToInit);
-
-  /**
-   * Run application login
-   */
-  virtual bool updateLoop(float fDelta) override;
-
-  /**
-   * Handle events from the server
-   */
-  virtual bool eventListener(lumin::ServerEvent* event) override;
-  bool pose6DofEventListener(lumin::ControlPose6DofInputEventData* event);
-  void urlBarEventListener();
-  bool gestureEventListener(lumin::GestureInputEventData* event);
-
-  /**
-   * Convert a point in prism coordinates to viewport coordinates
-   * (ignoring the z value).
-   */
-  glm::vec2 viewportPosition(glm::vec3 prism_pos);
-  bool pointInsideViewport(glm::vec2 pt);
-
-  /**
-   * Redraw the laser. Returns the laser endpoint, in viewport coordinates.
-   */
-  glm::vec2 redrawLaser();
+  int deInit();
+  
+  int getWidth() const;
+  void setWidth(int width);
+  int getHeight() const;
+  void setHeight(int height);
+  
+  ServoInstance *getInstance() const;
+  void flushTexture() const;
 
 private:
-  lumin::Prism* prism_ = nullptr;  // represents the bounded space where the App renders.
-  lumin::PlanarResource* plane_ = nullptr; // the plane we're rendering into
-  lumin::QuadNode* content_node_ = nullptr; // the node containing the plane
-  lumin::ui::UiPanel* content_panel_ = nullptr; // the panel containing the node
-  lumin::ui::UiButton* back_button_ = nullptr; // the back button
-  lumin::ui::UiButton* fwd_button_ = nullptr; // the forward button
-  lumin::ui::UiTextEdit* url_bar_ = nullptr; // the URL bar
-  lumin::LineNode* laser_ = nullptr; // The laser pointer
-  glm::vec3 controller_position_; // The last recorded position of the controller (in world coords)
-  glm::quat controller_orientation_; // The last recorded orientation of the controller (in world coords)
-  bool controller_trigger_down_ = false; // Is the controller trigger currently down?
-  ServoInstance* servo_ = nullptr; // the servo instance we're embedding
+  std::string url;
+  GLuint tex;
+  int width;
+  int height;
+  std::function<void()> onloadstart;
+  std::function<void(const std::string &)> onloadend;
+  std::function<void(int, const std::string &, const std::string &)> onloaderror;
+  std::function<void(const std::string &, const std::string &, int)> onconsole;
+  std::function<void(const std::string &)> onmessage;
+  
+  ServoInstance *servo_;
+  EGLDisplay display;
+  EGLSurface surface;
 };
