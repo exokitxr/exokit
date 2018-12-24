1a41c231ef64a6a0f0783509c58a3b6f48d78a40
avaer <a@modules.io>
2018-12-24 17:23:55 -0500

    Bugfix wrong browser blend aspect ratio in xr layers

--- src/index.js
+++ src/index.js
@@ -1077,16 +1077,17 @@ const _bindWindow = (window, newWindowCb) => {
     }
     
     const [{leftBounds, rightBounds}] = display.getLayers();
+    const {renderWidth, renderHeight} = display.getEyeParameters('left');
     const offsetMatrix = localMatrix2.compose(localVector.fromArray(layer.xrOffset.position), localQuaternion.fromArray(layer.xrOffset.orientation), localVector2.fromArray(layer.xrOffset.scale));
     
-    o.viewports[0][0] = leftBounds[0]*layer.width * factor;
-    o.viewports[0][1] = leftBounds[1]*layer.height;
-    o.viewports[0][2] = leftBounds[2]*layer.width * factor;
-    o.viewports[0][3] = leftBounds[3]*layer.height;
-    o.viewports[1][0] = rightBounds[0]*layer.width * factor;
-    o.viewports[1][1] = rightBounds[1]*layer.height;
-    o.viewports[1][2] = rightBounds[2]*layer.width * factor;
-    o.viewports[1][3] = rightBounds[3]*layer.height;
+    o.viewports[0][0] = leftBounds[0]*renderWidth * factor;
+    o.viewports[0][1] = leftBounds[1]*renderHeight;
+    o.viewports[0][2] = leftBounds[2]*renderWidth * factor;
+    o.viewports[0][3] = leftBounds[3]*renderHeight;
+    o.viewports[1][0] = rightBounds[0]*renderWidth * factor;
+    o.viewports[1][1] = rightBounds[1]*renderHeight;
+    o.viewports[1][2] = rightBounds[2]*renderWidth * factor;
+    o.viewports[1][3] = rightBounds[3]*renderHeight;
     localMatrix.fromArray(display._frameData.leftViewMatrix)
       .multiply(offsetMatrix)
       .toArray(o.modelView[0]);
