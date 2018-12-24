1d074d26a5e13065cd0432453614c2244c2cb2e6
avaer <a@modules.io>
2018-12-24 13:47:49 -0500

    Magic leap spacing cleanup

--- deps/exokit-bindings/magicleap/src/magicleap.cc
+++ deps/exokit-bindings/magicleap/src/magicleap.cc
@@ -2756,7 +2756,7 @@ NAN_METHOD(MLContext::Update) {
     Local<Object> xrOffset = Local<Object>::Cast(xrOffsetValue);
     Local<Float32Array> matrixFloat32Array = Local<Float32Array>::Cast(xrOffset->Get(JS_STR("matrix")));
     memcpy(transformMatrix.matrix_colmajor, (char *)matrixFloat32Array->Buffer()->GetContents().Data() + matrixFloat32Array->ByteOffset(), sizeof(transformMatrix.matrix_colmajor));
-    
+
     if (!isIdentityMatrix(transformMatrix)) {
       transformMatrix = invertMatrix(transformMatrix);
     }
