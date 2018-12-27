69580a6c72642c5187149b5b703a292a1d82dc9a
Avaer Kazmer <a@modules.io>
2018-12-27 15:04:27 -0500

    Bugfix magic leap build scripts relative paths

--- scripts/debug-ml.sh
+++ scripts/debug-ml.sh
@@ -6,4 +6,4 @@ cd "$(dirname "$0")"
 
 source ./version-ml.sh
 
-cmd.exe /c "$MLSDK_WIN/debug.cmd" --deploy-mpk build/magicleap/exokit.mpk build/magicleap/program-device/release_lumin_clang-3.8_aarch64/program-device --env "ARGS=node . $@"
+cmd.exe /c "$MLSDK_WIN/debug.cmd" --deploy-mpk ../build/magicleap/exokit.mpk ../build/magicleap/program-device/release_lumin_clang-3.8_aarch64/program-device --env "ARGS=node . $@"
