2943d0f98a9899ebedd658ee8fa3fe81df80abb5
avaer <a@modules.io>
2019-01-03 18:47:57 -0500

    Bugfix XRRigidTransform constructor orientation argument default

--- src/XR.js
+++ src/XR.js
@@ -514,7 +514,7 @@ class XRInputSourceEvent extends Event {
 module.exports.XRInputSourceEvent = XRInputSourceEvent;
 
 class XRRigidTransform {
-  constructor(position = {x: 0, y: 0, z: 0}, orientation = {x: 0, y: 0, z: 0, w: 0}, scale = {x: 1, y: 1, z: 1}) {
+  constructor(position = {x: 0, y: 0, z: 0}, orientation = {x: 0, y: 0, z: 0, w: 1}, scale = {x: 1, y: 1, z: 1}) {
     this.position = Float32Array.from([position.x, position.y, position.z]);
     this.orientation = Float32Array.from([orientation.x, orientation.y, orientation.z, orientation.w]);
     this.scale = Float32Array.from([scale.x, scale.y, scale.z]); // non-standard
