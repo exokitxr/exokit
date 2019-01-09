0472c037696511700a7033ae78a6b0a2b4df6005
Shane Harris <shane@xactaccounts.co.uk>
2019-01-09 00:02:58 +0000

    added string check to set clipboard method

--- deps/exokit-bindings/glfw/src/glfw.cc
+++ deps/exokit-bindings/glfw/src/glfw.cc
@@ -1352,7 +1352,7 @@ NAN_METHOD(GetClipboard) {
 
 NAN_METHOD(SetClipboard) {
   NATIVEwindow *window = GetCurrentWindowContext();
-  if (info.Length() == 1) {
+  if (info.Length() == 1 && info[0]->IsString()) {
     Nan::Utf8String utf8_value(info[0]);
     glfwSetClipboardString(window, *utf8_value);   
   } else {
