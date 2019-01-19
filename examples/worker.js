fe01e66cf595c90ea31f4b18975a542cf0fda75e
Avaer Kazmer <a@modules.io>
2019-01-19 12:50:20 -0500

    Update worker.html example

--- examples/worker.js
+++ examples/worker.js
@@ -1,9 +1,13 @@
-console.log('worker 1 loaded');
+console.log('worker 1');
 
 importScripts(self.location.pathname.split('/').slice(0,-1).join('/') + '/worker2.js');
 
+console.log('worker 2');
+
 onmessage = m => {
-  console.log('worker got message', m.data);
+  console.log('worker message 1', m.data);
 
   postMessage({world: true});
+
+  console.log('worker message 2', m.data);
 };
