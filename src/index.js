483cad0625620b7fee4b6acf72901922956d0d6e
Avaer Kazmer <a@modules.io>
2019-01-26 22:10:48 -0500

    Remove windows GlobalContext reference

--- src/index.js
+++ src/index.js
@@ -134,7 +134,6 @@ GlobalContext.contexts = contexts;
 const _windowHandleEquals = (a, b) => a[0] === b[0] && a[1] === b[1];
 
 const windows = [];
-GlobalContext.windows = windows;
 
 nativeBindings.nativeGl.onconstruct = (gl, canvas) => {
   const canvasWidth = canvas.width || innerWidth;
