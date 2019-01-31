d16ed1a7d4ce7c7a5c9f43053bbf4d79729c646f
Avaer Kazmer <a@modules.io>
2019-01-31 16:34:03 -0500

    Bugfix RTC binding.js

--- src/RTC/binding.js
+++ src/RTC/binding.js
@@ -1,4 +1,4 @@
 'use strict';
 
 const path = require('path');
-module.exports = require(path.join(__dirname, 'build', 'Release', 'wrtc.node));
+module.exports = require(path.join(__dirname, 'build', 'Release', 'wrtc.node'));
