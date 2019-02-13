e98649309bdedb12f6d11afaaa031badff703e2c
Avaer Kazmer <a@modules.io>
2019-02-13 01:27:23 +0000

    Small vrPresentState cleanup

--- src/index.js
+++ src/index.js
@@ -283,9 +283,8 @@ const vrPresentState = {
   fbo: null,
   tex: null,
   depthTex: null,
-  cleanups: null,
   hasPose: false,
-  lmContext: null,
+  // lmContext: null,
   layers: [],
 };
 GlobalContext.vrPresentState = vrPresentState;
