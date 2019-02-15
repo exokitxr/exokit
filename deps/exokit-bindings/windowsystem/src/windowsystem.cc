3bdf590be99ac8417f83f15daaa089d3f2451cde
Avaer Kazmer <a@modules.io>
2019-02-15 22:03:52 +0000

    Bugfix windowsystem MLMat4 usage

--- deps/exokit-bindings/windowsystem/src/windowsystem.cc
+++ deps/exokit-bindings/windowsystem/src/windowsystem.cc
@@ -4,6 +4,46 @@
 
 namespace windowsystembase {
 
+std::vector<float> multiplyMatrices(const std::vector<float> &a, const std::vector<float> &b) {
+  std::vector<float> result;
+  
+  const float *ae = a.data();
+  const float *be = b.data();
+  float *te = result.data();
+
+  float a11 = ae[ 0 ], a12 = ae[ 4 ], a13 = ae[ 8 ], a14 = ae[ 12 ];
+  float a21 = ae[ 1 ], a22 = ae[ 5 ], a23 = ae[ 9 ], a24 = ae[ 13 ];
+  float a31 = ae[ 2 ], a32 = ae[ 6 ], a33 = ae[ 10 ], a34 = ae[ 14 ];
+  float a41 = ae[ 3 ], a42 = ae[ 7 ], a43 = ae[ 11 ], a44 = ae[ 15 ];
+
+  float b11 = be[ 0 ], b12 = be[ 4 ], b13 = be[ 8 ], b14 = be[ 12 ];
+  float b21 = be[ 1 ], b22 = be[ 5 ], b23 = be[ 9 ], b24 = be[ 13 ];
+  float b31 = be[ 2 ], b32 = be[ 6 ], b33 = be[ 10 ], b34 = be[ 14 ];
+  float b41 = be[ 3 ], b42 = be[ 7 ], b43 = be[ 11 ], b44 = be[ 15 ];
+
+  te[ 0 ] = a11 * b11 + a12 * b21 + a13 * b31 + a14 * b41;
+  te[ 4 ] = a11 * b12 + a12 * b22 + a13 * b32 + a14 * b42;
+  te[ 8 ] = a11 * b13 + a12 * b23 + a13 * b33 + a14 * b43;
+  te[ 12 ] = a11 * b14 + a12 * b24 + a13 * b34 + a14 * b44;
+
+  te[ 1 ] = a21 * b11 + a22 * b21 + a23 * b31 + a24 * b41;
+  te[ 5 ] = a21 * b12 + a22 * b22 + a23 * b32 + a24 * b42;
+  te[ 9 ] = a21 * b13 + a22 * b23 + a23 * b33 + a24 * b43;
+  te[ 13 ] = a21 * b14 + a22 * b24 + a23 * b34 + a24 * b44;
+
+  te[ 2 ] = a31 * b11 + a32 * b21 + a33 * b31 + a34 * b41;
+  te[ 6 ] = a31 * b12 + a32 * b22 + a33 * b32 + a34 * b42;
+  te[ 10 ] = a31 * b13 + a32 * b23 + a33 * b33 + a34 * b43;
+  te[ 14 ] = a31 * b14 + a32 * b24 + a33 * b34 + a34 * b44;
+
+  te[ 3 ] = a41 * b11 + a42 * b21 + a43 * b31 + a44 * b41;
+  te[ 7 ] = a41 * b12 + a42 * b22 + a43 * b32 + a44 * b42;
+  te[ 11 ] = a41 * b13 + a42 * b23 + a43 * b33 + a44 * b43;
+  te[ 15 ] = a41 * b14 + a42 * b24 + a43 * b34 + a44 * b44;
+  
+  return result;
+}
+
 const char *composeVsh = ""
 #ifdef ANDROID
 "#version 300 es\n"
@@ -793,20 +833,20 @@ NAN_METHOD(ComposeLayers) {
             };
 
             Local<Float32Array> leftViewMatrixFloat32Array = xrStateObj->Get(JS_STR("leftViewMatrix"));
-            MLMat4f leftViewMatrix;
-            memcpy(leftViewMatrix.values, (float *)((char *)leftViewMatrixFloat32Array->GetContents().Data() + leftViewMatrixFloat32Array->ByteOffset()), sizeof(leftViewMatrix.values));
+            std::vector<float> leftViewMatrix;
+            memcpy(leftViewMatrix.data(), (float *)((char *)leftViewMatrixFloat32Array->GetContents().Data() + leftViewMatrixFloat32Array->ByteOffset()), sizeof(leftViewMatrix.data()));
 
             Local<Float32Array> rightViewMatrixFloat32Array = xrStateObj->Get(JS_STR("rightViewMatrix"));
-            MLMat4f rightViewMatrix;
-            memcpy(rightViewMatrix.values, (float *)((char *)rightViewMatrixFloat32Array->GetContents().Data() + rightViewMatrixFloat32Array->ByteOffset()), sizeof(rightViewMatrix.values));
+            std::vector<float> rightViewMatrix;
+            memcpy(rightViewMatrix.data(), (float *)((char *)rightViewMatrixFloat32Array->GetContents().Data() + rightViewMatrixFloat32Array->ByteOffset()), sizeof(rightViewMatrix.data()));
 
             Local<Value> xrOffsetValue = element->Get(JS_STR("xrOffset"));
             if (xrOffsetValue->BooleanValue()) {
               Local<Object> xrOffsetObj = Local<Object>::Cast(xrOffsetValue);
               Local<Float32Array> xrOffsetMatrixFloat32Array = Local<Float32Array>::Cast(xrOffsetObj->Get(JS_STR("matrix")));
 
-              MLMat4f xrOffsetMatrix;
-              memcpy(xrOffsetMatrix.values, (float *)(xrOffsetMatrixFloat32Array->GetContents().Data() + xrOffsetMatrixFloat32Array->ByteOffset()), sizeof(xrOffsetMatrix.values));
+              std::vector<float> xrOffsetMatrix;
+              memcpy(xrOffsetMatrix.data(), (float *)(xrOffsetMatrixFloat32Array->GetContents().Data() + xrOffsetMatrixFloat32Array->ByteOffset()), sizeof(xrOffsetMatrix.data()));
 
               leftViewMatrix = multiplyMatrices(leftViewMatrix, xrOffsetMatrix);
               rightViewMatrix = multiplyMatrices(rightViewMatrix, xrOffsetMatrix);
@@ -829,8 +869,8 @@ NAN_METHOD(ComposeLayers) {
                 rightViewport,
               },
               { // modelView
-                leftViewMatrix,
-                rightViewMatrix,
+                leftViewMatrix.data(),
+                rightViewMatrix.data(),
               },
               { // projection
                 leftProjectionMatrix,
