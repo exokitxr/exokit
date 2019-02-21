2e038d6a3f42598f595ac2d1f0a8d6fc8858bfdb
Avaer Kazmer <a@modules.io>
2019-02-21 16:56:28 -0500

    Bugfix tickAnimationFrame sync consumption from child windows

--- src/Window.js
+++ src/Window.js
@@ -1208,9 +1208,10 @@ const _normalizeUrl = utils._makeNormalizeUrl(options.baseUrl);
   };
   const _tickAnimationFrameRaf = top => async () => {
     const childSyncs = (await Promise.all(windows.map(window => window.tickAnimationFrame(GlobalContext.contexts.length > 0 ? 'child' : 'top')))).flat();
-    for (let i = 0; i < GlobalContext.contexts.length; i++) {
-      const context = GlobalContext.contexts[i];
-      context.setPrereqSyncs && context.setPrereqSyncs(childSyncs);
+    for (let i = 0; i < childSyncs.length; i++) {
+      const childSync = childSyncs[i];
+      nativeWindow.waitSync(childSync);
+      nativeWindow.deleteSync(childSync);
     }
 
     if (rafCbs.length > 0) {
@@ -1252,10 +1253,6 @@ const _normalizeUrl = utils._makeNormalizeUrl(options.baseUrl);
 
       context.clearPrereqSyncs && context.clearPrereqSyncs();
     }
-    for (let i = 0; i < childSyncs.length; i++) {
-      nativeWindow.deleteSync(childSyncs[i]);
-    }
-
     return syncs;
   };
   const _tickAnimationFrameTop = _tickAnimationFrameRaf(true);
