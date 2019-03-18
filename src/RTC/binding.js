aac361b2674f51345682831576a3bcedb654ad49
Avaer Kazmer <a@modules.io>
2019-03-18 13:07:52 -0400

    Remove old RTC js bindings

--- src/RTC/binding.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-const path = require('path');
-const bindings  = require(path.join(__dirname, '..', 'native-bindings'));
-module.exports = bindings.nativeRtc;
--- src/RTC/rtcrtptransceiver.js
+++
@@ -1,3 +0,0 @@
-class RTCRtpTransceiver {}
-
-module.exports = RTCRtpTransceiver;
