76acd727a9c1d2b8fe387022c111108e2dfde45b
Avaer Kazmer <a@modules.io>
2019-03-18 13:09:42 -0400

    Add node-webrtc RTC js bindings

---
+++ src/RTC/rtcvideosink.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var NativeRTCVideoSink = require('./binding').RTCVideoSink;
+var EventTarget = require('./eventtarget');
+
+function RTCVideoSink(track) {
+  EventTarget.call(this);
+
+  this._sink = new NativeRTCVideoSink(track);
+
+  var self = this;
+  this._sink.onframe = function onframe(frame) {
+    self.dispatchEvent({
+      type: 'frame',
+      frame: frame
+    });
+  };
+
+  Object.defineProperty(this, 'stopped', {
+    get: function() {
+      return self._sink.stopped;
+    }
+  });
+}
+
+RTCVideoSink.prototype.stop = function stop() {
+  return this._sink.stop();
+};
+
+module.exports = RTCVideoSink;
