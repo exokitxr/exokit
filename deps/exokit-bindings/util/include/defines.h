c162f0bcdc814c5b8b12a643737f6dcfe16e57a0
Avaer Kazmer <a@modules.io>
2019-03-23 19:34:13 -0400

    Small defines.h cleanup

--- deps/exokit-bindings/util/include/defines.h
+++ deps/exokit-bindings/util/include/defines.h
@@ -6,7 +6,7 @@
 
 using namespace v8;
 
-#define JS_STR(...) Nan::New<v8::String>(__VA_ARGS__).ToLocalChecked()
+#define JS_STR(val) Nan::New<v8::String>(val).ToLocalChecked()
 #define JS_INT(val) Nan::New<v8::Integer>(val)
 #define JS_NUM(val) Nan::New<v8::Number>(val)
 #define JS_FLOAT(val) Nan::New<v8::Number>(val)
