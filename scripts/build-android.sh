09097fa303e7d0cf8d7c3dd30b64701e114262e0
Avaer Kazmer <a@modules.io>
2019-04-05 05:22:26 -0400

    Add gen-dlibs flow to android build script

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -6,22 +6,21 @@ set -e
 
 # Preface.
 
-cd "$(dirname "$0")/.."
+export AR="/home/a/ndk/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android-ar"
 
-# pushd scripts
-# ./gen-assets-h.js $(pwd)/.. >../build/libexokit/assets.h 2>../build/libexokit/assets.h2
-# popd
+cd "$(dirname "$0")/.."
 
 rsync -a --copy-links package.json ./android/app/assets/package.json
 rsync -a --copy-links src/ ./android/app/assets/src
 rsync -a --copy-links lib/ ./android/app/assets/lib
 rsync -a --copy-links examples/ ./android/app/assets/examples
-rsync -a --copy-links node_modules/ ./android/app/assets/node_modules
+rsync -a --copy-links node_modules/* ./android/app/assets/node_modules/
+./scripts/gen-assets-h.js "$(pwd)" >android/app/assets/index.bin 2>android/app/assets/index.txt
 
-# mkdir -p build/libexokit
-pushd scripts
-./gen-assets-h.js $(pwd)/.. >../android/app/assets/index.bin 2>../android/app/assets/index.txt
-popd
+rm -Rf build/libexokit
+mkdir -p build/libexokit
+find build/Release/obj.target node_modules -name '*.o' | xargs "$AR" crs build/libexokit/libexokit.a
+./scripts/gen-dlibs-h.js "$(pwd)" >build/libexokit/dlibs.h
 
 pushd android
 ./gradlew build && ./gradlew assembleDebug
