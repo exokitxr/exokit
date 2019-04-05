767d9462da889950f866c2b78d865ed1fe27a500
Avaer Kazmer <a@modules.io>
2019-04-05 05:22:13 -0400

    Bugfix build-android and build-run scripts

--- scripts/run-android.sh
+++ scripts/run-android.sh
@@ -10,6 +10,8 @@ TOOLCHAIN="$PWD/../ndk"
 cd "$(dirname "$0")"
 
 pushd android
+adb shell am force-stop com.webmr.exokit
+adb shell am start -n com.webmr.exokit/android.app.NativeActivity
 adb logcat -c
 adb logcat -d | less
 popd
