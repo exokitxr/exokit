5e7fbe2a73d4a50727bd8f2abc6152883276ab06
Avaer Kazmer <a@modules.io>
2019-04-05 05:22:19 -0400

    Copy more directories in gen-assets script

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -12,7 +12,9 @@ pushd scripts
 ./gen-assets-h.js $(pwd)/.. >../build/libexokit/assets.h
 popd
 
+rsync -Pa package.json ./android/app/assets/package.json
 rsync -Pa src/ ./android/app/assets/src
+rsync -Pa examples/ ./android/app/assets/examples
 
 pushd android
 ./gradlew build && ./gradlew assembleDebug
