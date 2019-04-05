b87e6fe689fc3ddc601feb9d48fff084874305ae
Avaer Kazmer <a@modules.io>
2019-04-05 05:22:21 -0400

    Clean up build-android script copying

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -8,13 +8,21 @@ set -e
 
 cd "$(dirname "$0")/.."
 
+# pushd scripts
+# ./gen-assets-h.js $(pwd)/.. >../build/libexokit/assets.h 2>../build/libexokit/assets.h2
+# popd
+
+rsync -a --copy-links package.json ./android/app/assets/package.json
+rsync -a --copy-links src/ ./android/app/assets/src
+rsync -a --copy-links lib/ ./android/app/assets/lib
+rsync -a --copy-links examples/ ./android/app/assets/examples
+rsync -a --copy-links node_modules/ ./android/app/assets/node_modules
+
+mkdir -p build/libexokit
+
 pushd scripts
 ./gen-assets-h.js $(pwd)/.. >../build/libexokit/assets.h
 popd
-
-rsync -Pa package.json ./android/app/assets/package.json
-rsync -Pa src/ ./android/app/assets/src
-rsync -Pa examples/ ./android/app/assets/examples
 
 pushd android
 ./gradlew build && ./gradlew assembleDebug
