7e0e186a7b40ebc2cd70718b838be3992c4102a5
Avaer Kazmer <a@modules.io>
2019-04-07 05:40:41 -0400

    Add oculus-math source files

---
+++ deps/oculus-mobile/include/oculus-math.h
@@ -0,0 +1,12 @@
+#ifndef _OCULUS_MOBILE_MATH_H_
+#define _OCULUS_MOBILE_MATH_H_
+
+#include <oculus-mobile.h>
+
+namespace oculusmobile {
+
+ovrMatrix4f composeMatrix(const ovrVector3f &position, const ovrQuatf &quaternion, const ovrVector3f &scale);
+
+}
+
+#endif
---
+++ deps/oculus-mobile/src/oculus-math.cpp
@@ -0,0 +1,45 @@
+#ifdef ANDROID
+
+#include <oculus-math.h>
+
+namespace oculusmobile {
+
+ovrMatrix4f composeMatrix(const ovrVector3f &position, const ovrQuatf &quaternion, const ovrVector3f &scale) {
+  ovrMatrix4f result;
+
+  float	*te = (float *)&result.M[0][0]; // XXX row-major
+
+  float x = quaternion.x, y = quaternion.y, z = quaternion.z, w = quaternion.w;
+  float x2 = x + x,	y2 = y + y, z2 = z + z;
+  float xx = x * x2, xy = x * y2, xz = x * z2;
+  float yy = y * y2, yz = y * z2, zz = z * z2;
+  float wx = w * x2, wy = w * y2, wz = w * z2;
+
+  float sx = scale.x, sy = scale.y, sz = scale.z;
+
+  te[ 0 ] = ( 1 - ( yy + zz ) ) * sx;
+  te[ 1 ] = ( xy + wz ) * sx;
+  te[ 2 ] = ( xz - wy ) * sx;
+  te[ 3 ] = 0;
+
+  te[ 4 ] = ( xy - wz ) * sy;
+  te[ 5 ] = ( 1 - ( xx + zz ) ) * sy;
+  te[ 6 ] = ( yz + wx ) * sy;
+  te[ 7 ] = 0;
+
+  te[ 8 ] = ( xz + wy ) * sz;
+  te[ 9 ] = ( yz - wx ) * sz;
+  te[ 10 ] = ( 1 - ( xx + yy ) ) * sz;
+  te[ 11 ] = 0;
+
+  te[ 12 ] = position.x;
+  te[ 13 ] = position.y;
+  te[ 14 ] = position.z;
+  te[ 15 ] = 1;
+
+  return result;
+}
+
+}
+
+#endif
