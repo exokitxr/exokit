af4004c2ecd265f61aec1038def93a781188b257
Avaer Kazmer <a@modules.io>
2019-04-08 01:26:10 -0400

    Small index.js oculus mobile cleanup

--- src/index.js
+++ src/index.js
@@ -622,7 +622,6 @@ const oculusMobileVrPresentState = {
   depthTex: null,
   cleanups: null,
   hasPose: false,
-  lmContext: null,
   layers: [],
 };
 GlobalContext.oculusMobileVrPresentState = oculusMobileVrPresentState;
