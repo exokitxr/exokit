001b0382d0676a33b8d5666a25750195f249ee93
Chris <chris.eddy.dev@gmail.com>
2019-04-24 17:58:40 -0700

    mobile optimize launch page

--- launcher/src/components/Launch.jsx
+++ launcher/src/components/Launch.jsx
@@ -11,9 +11,9 @@ class Launch extends React.Component {
       return (
         <div id="Launch">
             <div className="row mt-4">
-                <div className="col-1">
+                <div className="col-lg-1 col-md-1 col-sm-0 col-xs-0">
                 </div>
-                <div className="col-5">
+                <div className="col-lg-5 col-md-5 col-sm-12 col-xs-12 m-4">
                     <div className="mb-4">
                         <h1>Reality Tab</h1>
                         <div className="form-group">
@@ -97,16 +97,16 @@ class Launch extends React.Component {
                     </div>
                     
                 </div>
-                <div className="col-5">
+                <div className="col-lg-5 col-md-5 col-sm-12 col-xs-12 m-4">
                     <div id="howto">
                         <h1>How To</h1>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
                         <p>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                     </div>
                 </div>
-                <div className="col-1">
+                <div className="col-lg-1 col-md-1 col-sm-0 col-xs-0">
                 </div>
-\            </div>
+            </div>
         </div>
         );
     }
