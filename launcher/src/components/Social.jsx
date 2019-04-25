c715e9a2f3385ebf90cea7b4832cec964fc45eda
Chris <chris.eddy.dev@gmail.com>
2019-04-24 21:02:41 -0700

    fix className

--- launcher/src/components/Social.jsx
+++ launcher/src/components/Social.jsx
@@ -13,19 +13,19 @@ class Social extends React.Component {
             <div className="row pb-5 mt-5">
               <div className="col-4 text-center">
                 <a href="https://twitter.com/webmixedreality?lang=en">
-                  <i class="fab fa-twitter fa-5x"></i>
+                  <i className="fab fa-twitter fa-5x"></i>
                   <h4 className="mt-3">Twitter</h4>
                 </a>
               </div>
               <div className="col-4 text-center">
                 <a href="https://www.twitch.tv/avaer">
-                  <i class="fab fa-twitch fa-5x"></i>
+                  <i className="fab fa-twitch fa-5x"></i>
                   <h4 className="mt-3">Twitch</h4>
                 </a>
               </div>
               <div className="col-4 text-center">
                 <a href="https://github.com/exokitxr/exokit">
-                  <i class="fab fa-github fa-5x"></i>
+                  <i className="fab fa-github fa-5x"></i>
                   <h4 className="mt-3">Github</h4>
                 </a>
               </div>
@@ -33,19 +33,19 @@ class Social extends React.Component {
             <div className="row pb-5 mt-5">
               <div className="col-4 text-center">
                 <a href="https://www.youtube.com/channel/UC87Q7_5ooY8FSLwOec52ZPQ">
-                <i class="fab fa-youtube fa-5x"></i>                  
+                <i className="fab fa-youtube fa-5x"></i>                  
                 <h4 className="mt-3">Youtube</h4>
                 </a>
               </div>
               <div className="col-4 text-center">
                 <a href="https://discordapp.com/invite/Apk6cZN">
-                <i class="fab fa-discord fa-5x"></i>
+                <i className="fab fa-discord fa-5x"></i>
                 <h4 className="mt-3">Discord</h4>
                 </a>
               </div>
               <div className="col-4 text-center">
                 <a href="https://communityinviter.com/apps/exokit/exokit">
-                <i class="fab fa-slack fa-5x"></i>
+                <i className="fab fa-slack fa-5x"></i>
                   <h4 className="mt-3">Slack</h4>
                 </a>
               </div>
