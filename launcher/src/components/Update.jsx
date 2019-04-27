c3cf115866254d1eb37984eaa3bced5c22845949
Chris <chris.eddy.dev@gmail.com>
2019-04-27 07:32:19 -0700

    setup postMessage to node server

--- launcher/src/components/Update.jsx
+++ launcher/src/components/Update.jsx
@@ -9,6 +9,7 @@ class Update extends React.Component {
       this.state = {
           readme: ''
         };
+        this.postMessage = this.postMessage.bind(this);
     }
 
     componentDidMount(){
@@ -20,6 +21,12 @@ class Update extends React.Component {
                     readme: data
                 });
             });
+    }
+
+    postMessage(){
+        window.postMessage({
+            click: 'hello world'
+        })
     }
 
     render() {
@@ -44,7 +51,7 @@ class Update extends React.Component {
                         <div className="progress-bar progress-bar-striped progress-bar-animated w-75" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
                     <br/>
-                    <button type="button" className="btn btn-primary btn-lg m-2">Update</button>
+                    <button onClick={() => this.postMessage()} type="button" className="btn btn-primary btn-lg m-2">Update</button>
                 </div>
                 <div className="col-lg-1 col-md-1 col-sm-1 col-xs-1">
                 </div>
