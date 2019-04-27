1ea5e37cccf04e9041b60aa42a70feac0cfad8de
Chris <chris.eddy.dev@gmail.com>
2019-04-27 14:13:06 -0700

    flags

--- launcher/backend.js
+++ launcher/backend.js
@@ -10,23 +10,40 @@ const process = require('os').platform();
         }
 
 
-    function launch(){
+    function launch(flags){
+        let flagsString = '-';
+        if(flags.length > 1){
+            flags.forEach(flag => {
+                flagsString += flag;
+            });
+        }
+        else{
+            flagsString += flag[0];
+        }
+        console.log('backend flags: ', flagsString)
         console.log('launching...')
-        const ls = spawn(exokitPath, []);
-
-        ls.stdout.on('data', (data) => {
-            console.log(`stdout: ${data}`);
-        });
+        const ls = spawn(exokitPath, [flagsString]);
         
-        ls.stderr.on('data', (data) => {
-            console.log(`stderr: ${data}`);
-        });
-        
-        ls.on('close', (code) => {
-            console.log(`child process exited with code ${code}`);
-        });
+    //         // arg2 is url, arg3 is flags, they can be empty or filled.
+    // if(arg3.length > 0 && arg2.length > 0){ // launch with both flag and url
+    //     spawn(exokitPath, [arg2, '-' + arg3], {detached: true, stdio: ['ignore', 'ignore', 'ignore']});
+    //   }
+    //   if(arg3.length > 0){ // launch with just a flag
+    //     spawn(exokitPath, ['-' + arg3], {detached: true, stdio: ['ignore', 'ignore', 'ignore']});
+    //   }
+    //   if(arg2.length > 0){ // launch with just a URL parameter
+    //     spawn(exokitPath, [arg2], {detached: true, stdio: ['ignore', 'ignore', 'ignore']});
+    //   }
+    //   else{ // launch with nothing extra
+    //     if(process.platform === 'darwin'){
+    //       spawn('sh', [exokitPath], {shell: true, detached: true});
+    //     }
+    //     else{
+    //       spawn(exokitPath, {detached: true, stdio: ['ignore', 'ignore', 'ignore']});
+    //     }
+    //   }
     }
-    
+
     function update(){
         console.log('updating...')
     }
