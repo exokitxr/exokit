bded7d5f97cabe6331744669e0f39ec1baa34422
Chris <chris.eddy.dev@gmail.com>
2019-04-27 19:10:53 -0700

    fix file extensions

--- launcher/backend.js
+++ launcher/backend.js
@@ -1,58 +1,70 @@
 const { spawn } = require('child_process');
 const os = require('os');
 const platform = os.platform();
+const process = require('process');
+const path = require('path');
 
 const fs = require('fs');
-const https = require('https');
+const http = require('http');
 
-let exokitPath;
-if(process.platform === 'win32'){
-    exokitPath = 'scripts\\exokit.cmd';
-}
-else{
-    exokitPath = 'scripts/exokit.sh';
-}
-
-console.log(os);
-
-function launch(flags){
+function launch(flags, url){
     console.log('launching...')
-    let flagsString = '-';
+    console.log('flags', flags)
+    let flagsArray = ['--experimental-worker', '.', url];
+    let flagsString = '';
     if(flags.length > 1){
+        flagsString += '-';
         flags.forEach(flag => {
             flagsString += flag;
         });
+        flagsArray.push(flagsString);
     }
-    else{
-        flagsString += flag[0];
+    else if(flags.length == 1){
+        flagsString += '-';
+        flagsString += flags[0];
+        flagsArray.push(flagsString);
     }
-    spawn(exokitPath, [flagsString]);
-}
+    console.log(flagsArray);
+    const ls = spawn('node', flagsArray, {detached: true});
 
+    ls.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
+    
+    ls.stderr.on('data', (data) => {
+        console.log(`stderr: ${data}`);
+    });
+    
+    ls.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
+}
 
 function update(){
     console.log('updating...')
 
     const downloadPath = '/Users/chris/Downloads/'
+    let extension = '';
+    let url = '';
 
-    let writeStream = fs.createWriteStream(downloadPath + 'exokit-installer.exe');
-    let url = 'https://get.webmr.io/macos';
-
-    console.log('Detected OS:', process.platform);
-
-    switch (process.platform) {
+    switch (platform) {
         case 'win32':
-            url = 'https://get.webmr.io/windows';
+            url = 'http://get.webmr.io/windows';
+            extension = '.exe';
             break;
         case 'darwin':
-            url = 'https://get.webmr.io/macos';
+            url = 'http://get.webmr.io/macos';
+            extension = '.dmg';
             break;
         case 'linux':
-            url = 'https://get.webmr.io/linux';
+            url = 'http://get.webmr.io/linux';
+            extension = '.bin';
             break;
     }
 
-    https.get(url, (res) => {
+    let writeStream = fs.createWriteStream(downloadPath + 'exokit-installer'  + extension);
+    
+    http.get(url, (res) => {
 
       console.log('Downloading Exokit...');
 
