54f9965ea7ac21706745727e62e41be516d013bb
Avaer Kazmer <a@modules.io>
2019-04-27 22:14:51 -0400

    Update launcher source

--- launcher/src/components/Launch.jsx
+++ launcher/src/components/Launch.jsx
@@ -5,6 +5,37 @@ class Launch extends React.Component {
 
     constructor(props) {
       super(props);
+      this.postMessage = this.postMessage.bind(this);
+      this.setFlag = this.setFlag.bind(this);
+      this.handleURLChange = this.handleURLChange.bind(this);
+      this.state = {
+          flags: [],
+          url: ''
+      };
+    }
+
+    postMessage(action){
+        window.postMessage({
+            action: action,
+            flags: this.state.flags,
+            url: this.state.url
+        })
+    }
+
+    handleURLChange(e){
+        this.setState({
+            url: e.target.value
+        })
+    }
+
+    setFlag(e){
+        let flag = e.target.value;
+        if(!this.state.flags.includes(flag)){
+            this.state.flags.push(flag)
+        }
+        else{
+            this.state.flags.splice(this.state.flags.indexOf(flag), 1);
+        }
     }
 
     render() {
@@ -17,80 +48,80 @@ class Launch extends React.Component {
                     <div className="mb-4">
                         <h1>Reality Tab</h1>
                         <div className="form-group">
-                            <input type="text" className="form-control" id="urlInput" placeholder="Enter URL"/>
+                            <input onChange={(e) => this.handleURLChange(e)} type="text" className="form-control" id="urlInput" placeholder="Enter URL"/>
                         </div>
-                        <button type="button" className="btn btn-primary">Launch</button>
+                        <button onClick={() => this.postMessage('launch')} type="button" className="btn btn-primary">Launch</button>
                     </div>
 
                     <h3>Flags</h3>
                     <div className="form-check">
-                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+                        <input onChange={(e) => this.setFlag(e)} className="form-check-input" type="checkbox" value="u" id="defaultCheck1"/>
                         <label className="form-check-label">
                             <code>--uncapped</code> - Uncapped FPS
                         </label>
                     </div>
                     <div className="form-check">
-                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+                        <input onChange={(e) => this.setFlag(e)} className="form-check-input" type="checkbox" value="p" id="defaultCheck1"/>
                         <label className="form-check-label">
                             <code>--performance</code> - Log Performance
                         </label>
                     </div>
                     <div className="form-check">
-                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+                        <input onChange={(e) => this.setFlag(e)} className="form-check-input" type="checkbox" value="w" id="defaultCheck1"/>
                         <label className="form-check-label">
                             <code>--webgl</code> - WebGL
                         </label>
                     </div>
                     <div className="form-check">
-                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+                        <input onChange={(e) => this.setFlag(e)} className="form-check-input" type="checkbox" value="x" id="defaultCheck1"/>
                         <label className="form-check-label">
                             <code>--xr</code> - WebXR
                         </label>
                     </div>
                     <div className="form-check">
-                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+                        <input onChange={(e) => this.setFlag(e)} className="form-check-input" type="checkbox" value="b" id="defaultCheck1"/>
                         <label className="form-check-label">
                             <code>--blit</code> - Blit
                         </label>
                     </div>
                     <div className="form-check">
-                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+                        <input onChange={(e) => this.setFlag(e)} className="form-check-input" type="checkbox" value="t" id="defaultCheck1"/>
                         <label className="form-check-label">
                             <code>--tab</code> - Reality Tab
                         </label>
                     </div>
                     <div className="form-check">
-                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+                        <input onChange={(e) => this.setFlag(e)} className="form-check-input" type="checkbox" value="s" id="defaultCheck1"/>
                         <label className="form-check-label">
                             <code>--size</code> - Size
                         </label>
                     </div>
                     <div className="form-check">
-                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+                        <input onChange={(e) => this.setFlag(e)} className="form-check-input" type="checkbox" value="f" id="defaultCheck1"/>
                         <label className="form-check-label">
                             <code>--frame</code> - Frame
                         </label>
                     </div>
                     <div className="form-check">
-                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+                        <input onChange={(e) => this.setFlag(e)} className="form-check-input" type="checkbox" value="m" id="defaultCheck1"/>
                         <label className="form-check-label">
                             <code>--minimalFrame</code> - Minimal Frame
                         </label>
                     </div>
                     <div className="form-check">
-                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+                        <input onChange={(e) => this.setFlag(e)} className="form-check-input" type="checkbox" value="q" id="defaultCheck1"/>
                         <label className="form-check-label">
                             <code>--quit</code> - Quit
                         </label>
                     </div>
                     <div className="form-check">
-                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+                        <input onChange={(e) => this.setFlag(e)} className="form-check-input" type="checkbox" value="r" id="defaultCheck1"/>
                         <label className="form-check-label">
                             <code>--require</code> - Require
                         </label>
                     </div>
                     <div className="form-check">
-                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+                        <input onChange={(e) => this.setFlag(e)} className="form-check-input" type="checkbox" value="i" id="defaultCheck1"/>
                         <label className="form-check-label">
                             <code>--image</code> - Image
                         </label>
