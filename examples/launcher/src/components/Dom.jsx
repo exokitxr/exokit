83b286859e31b2fdc0f6ceaaea08ab864718164c
Avaer Kazmer <a@modules.io>
2019-05-04 00:45:56 -0400

    Add initial studio DOM editing support

--- examples/launcher/src/components/Dom.jsx
+++ examples/launcher/src/components/Dom.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import '../css/dom.css';
 
+const _decorateKeypaths = (el, keypath = []) => {
+  el.keypath = keypath;
+  for (let i = 0; i < el.childNodes.length; i++) {
+    _decorateKeypaths(el.childNodes[i], keypath.concat([i]));
+  }
+};
 const _el2Text = el => {
   let result = '<' + el.tagName.toLowerCase();
   for (let i = 0; i < el.attrs.length; i++) {
@@ -14,31 +20,33 @@ const _el2Text = el => {
 class Dom extends React.Component {
   constructor(props) {
     super(props);
-    
+
+    const root = {
+      nodeType: 1,
+      tagName: 'HTML',
+      value: '',
+      attrs: [{
+        name: 'href',
+        value: 'https://',
+      }],
+      childNodes: [
+        {
+          nodeType: 1,
+          tagName: 'BODY',
+          value: '',
+          attrs: [{
+            name: 'lol',
+            value: 'zol',
+            name: 'woot',
+            value: 'toot',
+          }],
+          childNodes: [],
+        },
+      ],
+    };
+    _decorateKeypaths(root);
     this.state = {
-      root: {
-        nodeType: 1,
-        tagName: 'HTML',
-        value: '',
-        attrs: [{
-          name: 'href',
-          value: 'https://',
-        }],
-        childNodes: [
-          {
-            nodeType: 1,
-            tagName: 'BODY',
-            value: '',
-            attrs: [{
-              name: 'lol',
-              value: 'zol',
-              name: 'woot',
-              value: 'toot',
-            }],
-            childNodes: [],
-          },
-        ],
-      },
+      root,
       selectEl: null,
       epoch: 0,
     };
@@ -48,6 +56,7 @@ class Dom extends React.Component {
     window.addEventListener('message', e => {
       const m = e.data;
       if (m.method === 'dom') {
+        _decorateKeypaths(m.root);
         this.setState({
           root: m.root,
         });
@@ -190,7 +199,7 @@ class DomDetail extends React.Component {
     return (
       <div className="dom-detail">
         <div className="dom-detail-name">{el.tagName.toLowerCase()}</div>
-        {el.attrs.map(attr => <DomAttribute attr={attr} key={attr.name} epoch={this.props.epoch} />)}
+        {el.attrs.map(attr => <DomAttribute name={attr.name} value={attr.value} keypath={el.keypath} epoch={this.props.epoch} key={attr.name} />)}
       </div>
     );
   }
@@ -201,14 +210,14 @@ class DomAttribute extends React.Component {
     super(props);
 
     this.state = {
-      name: props.attr.name,
-      value: props.attr.value,
+      name: props.name,
+      value: props.value,
     };
   }
   
   UNSAFE_componentWillUpdate(prevProps, prevState) {
     if (this.props.epoch !== prevProps.epoch) {
-      const {props: {attr: {name, value}}} = this;
+      const {name, value} = this.props;
       this.setState({
         name,
         value,
@@ -222,6 +231,19 @@ class DomAttribute extends React.Component {
       name,
     });
   }
+  
+  onNameBlur() {
+    window.postMessage({
+      method: 'edit',
+      keypath: this.props.keypath,
+      edit: {
+        type: 'name',
+        oldName: this.props.name,
+        oldValue: this.props.value,
+        newName: this.state.name,
+      },
+    });
+  }
 
   onValueChange(e) {
     const value = e.target.value;
@@ -229,12 +251,24 @@ class DomAttribute extends React.Component {
       value,
     });
   }
+  
+  onValueBlur() {
+    window.postMessage({
+      method: 'edit',
+      keypath: this.props.keypath,
+      edit: {
+        type: 'value',
+        name: this.props.name,
+        newValue: this.state.value,
+      },
+    });
+  }
 
   render() {
     return (
       <div className="dom-attribute">
-        <input type="text" className="dom-attribute-name" value={this.state.name} onChange={e => this.onNameChange(e)} />
-        <input type="text" className="dom-attribute-value" value={this.state.value} onChange={e => this.onValueChange(e)} />
+        <input type="text" className="dom-attribute-name" value={this.state.name} onChange={e => this.onNameChange(e)} onBlur={e => this.onNameBlur()} />
+        <input type="text" className="dom-attribute-value" value={this.state.value} onChange={e => this.onValueChange(e)} onBlur={e => this.onNameBlur()} />
       </div>
     );
   }
