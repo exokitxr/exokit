7393da96167cb6aab3b217b020c6b627c5c6b850
Avaer Kazmer <a@modules.io>
2019-05-05 21:07:38 -0400

    Studio App.js dead code cleanup

--- examples/launcher/src/App.js
+++ examples/launcher/src/App.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import './css/App.css';
-// import NavTabs from './components/NavTabs.jsx';
 import Engine from './components/Engine.jsx'
-import Update from './components/Update.jsx'
-import Launch from './components/Launch';
-import Social from './components/Social';
 
 function App() {
   return (
     <div className="App" id="app">
-      {/* <NavTabs/> */}
       <Engine/>
-      {/* <Launch/>
-      <Update/>
-      <Social/> */}
     </div>
   );
 }
