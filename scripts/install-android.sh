316f214c729bd37ffc3bce6186739674eec3a623
Avaer Kazmer <a@modules.io>
2019-05-15 14:34:19 -0400

    More android install scripts debugging

--- scripts/install-android.sh
+++ scripts/install-android.sh
@@ -6,13 +6,11 @@ set -e
 
 # Preface.
 
+cd "$(dirname "$0")"
+
 source ./version-android.sh
 
-cd "$(dirname "$0")/.."
-
-export ANDROID_HOME=${ANDROID_HOME:-/home/a/sdk}
-
-pushd android
+pushd ../android
 "$ANDROID_HOME/platform-tools/adb" install -r ./app/build/outputs/apk/debug/app-debug.apk
 # ./gradlew installDebug
 popd
