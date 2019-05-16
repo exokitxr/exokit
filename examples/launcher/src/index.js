5c512c58ad2632c8c5911e5788dc47d452e1d25d
chrisplatorres <me@chrislatorres.com>
2019-05-16 16:01:54 -0500

    on launcher index return false oncontextmenu

--- examples/launcher/src/index.js
+++ examples/launcher/src/index.js
@@ -3,5 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+document.oncontextmenu = function() {
+    return false;
+}
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
