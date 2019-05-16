b9134c8568793cfe7b7523cd557e0992d520d652
chrisplatorres <me@chrislatorres.com>
2019-05-16 16:18:04 -0500

    add oncontextmenu to launcher app.js

--- examples/launcher/src/App.js
+++ examples/launcher/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './css/App.css';
 import Engine from './components/Engine.jsx'
 
+document.oncontextmenu = function() {
+    return false;
+}
 function App() {
   return (
     <div className="App" id="app">
