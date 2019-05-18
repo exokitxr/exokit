e3ca005b55651493cf0514aa2e5e168539ec0f42
Avaer Kazmer <a@modules.io>
2019-05-18 16:30:34 -0400

    Bugfix android-run script version injection

--- scripts/run-android.sh
+++ scripts/run-android.sh
@@ -6,15 +6,15 @@ set -e
 
 # Preface.
 
+
+cd "$(dirname "$0")"
+
 source ./version-android.sh
 
-cd "$(dirname "$0")/.."
-
-pushd android
-
+pushd ../android
 "$ANDROID_HOME/platform-tools/adb" shell am force-stop com.webmr.exokit
 "$ANDROID_HOME/platform-tools/adb" logcat -c
 "$ANDROID_HOME/platform-tools/adb" shell am start -n com.webmr.exokit/android.app.NativeActivity
-# "$ANDROID_HOME/platform-tools/adb" shell am start -n com.webmr.exokit/android.app.NativeActivity -e ARGS "'node --experimental-worker /package /package/examples/tutorial.html'"
+# "$ANDROID_HOME/platform-tools/adb" shell am start -n com.webmr.exokit/android.app.NativeActivity -e ARGS "'node --inspect-brk /package /package/examples/tutorial.html'"
 "$ANDROID_HOME/platform-tools/adb" logcat
 popd
