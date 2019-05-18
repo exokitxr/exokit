5cc8869f441607b2ccd794b7105dd579ea2a955f
Avaer Kazmer <a@modules.io>
2019-05-18 13:42:59 -0400

    Add ARGS passing example to run-android script

--- scripts/run-android.sh
+++ scripts/run-android.sh
@@ -15,5 +15,6 @@ pushd android
 "$ANDROID_HOME/platform-tools/adb" shell am force-stop com.webmr.exokit
 "$ANDROID_HOME/platform-tools/adb" logcat -c
 "$ANDROID_HOME/platform-tools/adb" shell am start -n com.webmr.exokit/android.app.NativeActivity
+# "$ANDROID_HOME/platform-tools/adb" shell am start -n com.webmr.exokit/android.app.NativeActivity -e ARGS "'node --experimental-worker /package /package/examples/tutorial.html'"
 "$ANDROID_HOME/platform-tools/adb" logcat
 popd
