04d888685ab52cfeed3b217f9eedf835cc485fd4
chrisplatorres <me@chrislatorres.com>
2019-05-19 16:14:54 -0500

    remove duplicate scripts

--- scripts/make-toolchain-android.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Initialization.
-
-set -e
-
-# Make standalone toolchain 
-
-$ANDROID_HOME/ndk-bundle/build/tools/make_standalone_toolchain.py --arch arm64 --api 21 --install-dir $ANDROID_HOME/ndk-bundle/toolchain
