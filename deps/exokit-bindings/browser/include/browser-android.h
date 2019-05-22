efe961960ce3d255920cd111a8e9e67ba87f37bb
Avaer Kazmer <a@modules.io>
2019-05-22 04:39:47 -0400

    Add initial browser-android.h source

---
+++ deps/exokit-bindings/browser/include/browser-android.h
@@ -0,0 +1,21 @@
+#ifndef _BROWSER_ANDROID_H_
+#define _BROWSER_ANDROID_H_
+
+#include <v8.h>
+#include <node.h>
+#include <nan.h>
+
+#include <webgl.h>
+#include <browser-common.h>
+
+using namespace std;
+using namespace v8;
+using namespace node;
+
+namespace browser {
+
+// XXX
+
+}
+
+#endif
