ad9ecadad0aa8e1f7e2c17646939c4320460ca29
chrisplatorres <me@chrislatorres.com>
2019-05-22 16:52:44 -0500

    uncomment DomAttribte and DomDetail

--- examples/launcher/src/components/Dom.jsx
+++ examples/launcher/src/components/Dom.jsx
@@ -265,133 +265,96 @@ class DomItem extends React.Component {
   }
 }
 
-// class DomDetail extends React.Component {
-//   /* constructor(props) {
-//     super(props);
-//   } */
-//   toggleOpen() {
-//     const {el} = this.props;
-//
-//     this.setState({
-//       open: !this.state.open,
-//     });
-//
-//     el.stopPropagation();
-//   }
-//
-//   cloneTab() {
-//     const {el} = this.props;
-//     let url;
-//
-//     el.attrs.map(attr => (attr.name === "src") ? url = attr.value : null);
-//
-//     window.postMessage({
-//       method: 'open',
-//       url,
-//       d: 3,
-//     });
-//
-//     this.toggleOpen();
-//   }
-//
-//   deleteTab() {
-//     const {el} = this.props;
-//
-//     window.postMessage({
-//       method: 'edit',
-//       keypath: el.keypath,
-//       edit: {
-//         type: 'remove',
-//       },
-//     });
-//
-//     this.toggleOpen();
-//   }
-//   render() {
-//     const {el} = this.props;
-//
-//     return (
-//       <div className="dom-detail">
-//         <div className="dom-detail-button" onClick={() => this.cloneTab()}>Clone</div>
-//         <div className="dom-detail-button" onClick={() => this.deleteTab()}>Delete</div>
-//       </div>
-//     );
-//   }
-// }
+class DomDetail extends React.Component {
+  /* constructor(props) {
+    super(props);
+  } */
 
-// class DomAttribute extends React.Component {
-//   constructor(props) {
-//     super(props);
-//
-//     this.state = {
-//       name: props.name,
-//       value: props.value,
-//     };
-//   }
-//
-//   UNSAFE_componentWillUpdate(prevProps, prevState) {
-//     if (this.props.epoch !== prevProps.epoch) {
-//       const {name, value} = this.props;
-//       this.setState({
-//         name,
-//         value,
-//       });
-//     }
-//   }
-//
-//   onKeyDown(e) {
-//     if (e.code === 13) { // enter
-//       document.activeElement.blur();
-//     }
-//   }
-//
-//   onNameChange(e) {
-//     const name = e.target.value;
-//     this.setState({
-//       name,
-//     });
-//   }
-//
-//   onNameBlur() {
-//     window.postMessage({
-//       method: 'edit',
-//       keypath: this.props.keypath,
-//       edit: {
-//         type: 'name',
-//         oldName: this.props.name,
-//         oldValue: this.props.value,
-//         newName: this.state.name,
-//       },
-//     });
-//   }
-//
-//   onValueChange(e) {
-//     const value = e.target.value;
-//     this.setState({
-//       value,
-//     });
-//   }
-//
-//   onValueBlur() {
-//     window.postMessage({
-//       method: 'edit',
-//       keypath: this.props.keypath,
-//       edit: {
-//         type: 'value',
-//         name: this.props.name,
-//         newValue: this.state.value,
-//       },
-//     });
-//   }
-//
-//   render() {
-//     return (
-//       <div className="dom-attribute">
-//         <input type="text" className="dom-attribute-name" value={this.state.name} onKeyDown={e => this.onKeyDown(e)} onChange={e => this.onNameChange(e)} onBlur={e => this.onNameBlur()} />
-//         <input type="text" className="dom-attribute-value" value={this.state.value} onKeyDown={e => this.onKeyDown(e)} onChange={e => this.onValueChange(e)} onBlur={e => this.onValueBlur()} />
-//       </div>
-//     );
-//   }
-// }
+  render() {
+    const {el} = this.props;
+
+    return (
+      <div className="dom-detail">
+        <div className="dom-detail-name">{el.tagName.toLowerCase()}</div>
+        {el.attrs.map(attr => <DomAttribute name={attr.name} value={attr.value} keypath={el.keypath} epoch={this.props.epoch} key={attr.name} />)}
+      </div>
+    );
+  }
+}
+
+class DomAttribute extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: props.name,
+      value: props.value,
+    };
+  }
+
+  UNSAFE_componentWillUpdate(prevProps, prevState) {
+    if (this.props.epoch !== prevProps.epoch) {
+      const {name, value} = this.props;
+      this.setState({
+        name,
+        value,
+      });
+    }
+  }
+
+  onKeyDown(e) {
+    if (e.code === 13) { // enter
+      document.activeElement.blur();
+    }
+  }
+
+  onNameChange(e) {
+    const name = e.target.value;
+    this.setState({
+      name,
+    });
+  }
+
+  onNameBlur() {
+    window.postMessage({
+      method: 'edit',
+      keypath: this.props.keypath,
+      edit: {
+        type: 'name',
+        oldName: this.props.name,
+        oldValue: this.props.value,
+        newName: this.state.name,
+      },
+    });
+  }
+
+  onValueChange(e) {
+    const value = e.target.value;
+    this.setState({
+      value,
+    });
+  }
+
+  onValueBlur() {
+    window.postMessage({
+      method: 'edit',
+      keypath: this.props.keypath,
+      edit: {
+        type: 'value',
+        name: this.props.name,
+        newValue: this.state.value,
+      },
+    });
+  }
+
+  render() {
+    return (
+      <div className="dom-attribute">
+        <input type="text" className="dom-attribute-name" value={this.state.name} onKeyDown={e => this.onKeyDown(e)} onChange={e => this.onNameChange(e)} onBlur={e => this.onNameBlur()} />
+        <input type="text" className="dom-attribute-value" value={this.state.value} onKeyDown={e => this.onKeyDown(e)} onChange={e => this.onValueChange(e)} onBlur={e => this.onValueBlur()} />
+      </div>
+    );
+  }
+}
 
 export default Dom;
