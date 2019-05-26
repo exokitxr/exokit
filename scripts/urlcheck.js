e9ca7bc4999aa1348dbb885fdaf954dc1367eede
chrisplatorres <me@chrislatorres.com>
2019-05-26 12:28:17 -0500

    Revert "add script to check for and filter out custom exokit:// url"
    This reverts commit bde773d900a45c057a10125bdd13ba66ddc98a8b.

--- scripts/urlcheck.js
+++
@@ -1,8 +0,0 @@
-var args = process.argv.slice(2);
-if (args[0].includes("exokit://")){
-  result = args[0].replace(/(^\w+:|^)\/\/\w+\/\//, '');
-  result = 'https://' + result;
-  console.log(result);
-} else {
-  console.log(args[0]);
-}
