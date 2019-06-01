ffc2c1d3b3c7274d20019deb5e21d833556e7d56
Avaer Kazmer <a@modules.io>
2019-06-01 10:30:45 -0400

    Bugfix tickAnimationFrame handling in WindowBase

--- src/WindowBase.js
+++ src/WindowBase.js
@@ -315,7 +315,7 @@ parentPort.on('message', m => {
     case 'runAsync': {
       let result, err;
       try {
-        result = window.onrunasync ? window.onrunasync(m.jsString) : null;
+        result = window.onrunasync ? window.onrunasync(m.request) : null;
       } catch(e) {
         err = e.stack;
       }
