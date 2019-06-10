382a7b22bb8cf00fea604bf5a38ef6c616db84e1
Chris <chris.eddy.dev@gmail.com>
2019-06-10 14:04:08 -0700

    create hackCI script, package.json fetch

--- hackCI.js
+++ hackCI.js
@@ -1,16 +1,76 @@
 const { spawn } = require('child_process');
-const ls = spawn('npm', ['install', '-g', 'appdmg']);
- 
-console.log('Testing CI hack', process.env)
+const fetch = require('node-fetch');
+const _ =  require('lodash');
 
-ls.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
-});
+let getPrevHash = new Promise(function(resolve, reject) {
+  const prevHash = spawn('git', ['rev-parse', 'HEAD~1']);
+  prevHash.stdout.on('data', (data) => {
+    console.log(`Prev Hash: ${data}`);
+    resolve(data);
+  });
+  prevHash.stderr.on('data', (data) => {
+    console.log(`stderr: ${data}`);
+    reject()
+  });
+  prevHash.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+})
 
-ls.stderr.on('data', (data) => {
-  console.log(`stderr: ${data}`);
-});
+let getCurrentHash = new Promise(function(resolve, reject) {
+  const prevHash = spawn('git', ['rev-parse', 'HEAD']);
+  prevHash.stdout.on('data', (data) => {
+    console.log(`Current Hash: ${data}`);
+    resolve(data);
+  });
+  prevHash.stderr.on('data', (data) => {
+    console.log(`stderr: ${data}`);
+    reject()
+  });
+  prevHash.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+})
 
-ls.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
-});
+async function main(){
+  let prevHash = await getPrevHash;
+  let currentHash = await getCurrentHash;
+  prevHash = prevHash.toString();
+  currentHash = prevHash.toString();
+  // GET PREVIOUS COMMIT PACKAGE.JSON
+  fetch('https://raw.githubusercontent.com/exokitxr/exokit/' + prevHash.slice(0, -3) + '/package.json',)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    const prevPackageJSON = json;
+    // GET CURRENT COMMIT PACKAGE.JSON
+      fetch('https://raw.githubusercontent.com/exokitxr/exokit/' + currentHash.slice(0, -3) + '/package.json',)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      const currentPackageJSON = json;
+      // COMPARE PACKAGE.JSON's, could be more specific but for now just compare full file.
+      if(_.isEqual(prevPackageJSON, currentPackageJSON)){
+        console.log('Skipping install');
+      }
+      else{
+        // NPM INSTALL / INSTALL STUFF HERE
+        const npmInstall = spawn('npm', ['install', '-g', 'appdmg']);
+        npmInstall.stdout.on('data', (data) => {
+          console.log(data);
+        });
+        npmInstall.stderr.on('data', (data) => {
+          console.log(`stderr: ${data}`);
+        });
+        npmInstall.on('close', (code) => {
+          console.log(`child process exited with code ${code}`);
+        });
+      }
+    })
+  })
+}
+
+main();
+
