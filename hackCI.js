012fcde2d4f4978189b1b4ee8bee58371f72025e
Chris <chris.eddy.dev@gmail.com>
2019-06-09 18:44:41 -0700

    test JS file npm i spawn

---
+++ hackCI.js
@@ -0,0 +1,18 @@
+const { spawn } = require('child_process');
+const ls = spawn('npm', ['install', '-g', 'appdmg']);
+
+console.log('Testing CI hack')
+
+ls.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+ls.stderr.on('data', (data) => {
+  console.log(`stderr: ${data}`);
+});
+
+ls.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
+
+return true;
