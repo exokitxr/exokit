4e6a7eb3621b708f97467f3a34eaa6b1e6f2f690
Avaer Kazmer <a@modules.io>
2019-06-22 12:07:37 -0400

    Major browser-android work

--- deps/exokit-bindings/browser/include/browser-android.h
+++ deps/exokit-bindings/browser/include/browser-android.h
@@ -18,18 +18,40 @@ using namespace node;
 
 namespace browser {
 
-class Browser : public ObjectWrap {
+class Browser {
 public:
-  Browser(WebGLRenderingContext *gl, int width, int height, const std::string &urlString);
+  Browser(GLuint externalTex, GLuint tex, int width, int height, const std::string &urlString);
   ~Browser();
+
+  GLuint externalTex;
+  GLuint tex;
+
+  GLuint renderFbo;
+  GLuint renderVao;
+  GLuint renderProgram;
+
+  GLuint renderVertex;
+  GLuint renderFragment;
+  GLuint positionLocation;
+};
+
+class BrowserWrap : public ObjectWrap {
+public:
+  BrowserWrap(Browser *browser);
+  ~BrowserWrap();
 
   static NAN_METHOD(New);
   static NAN_GETTER(TextureGetter);
 
   static Local<Object> Initialize(Isolate *isolate);
 
-  GLuint tex;
+  Browser *browser;
 };
+
+extern std::vector<std::function<void()>> mainThreadFns;
+extern std::vector<std::function<void()>> persistentMainThreadFns;
+void RunInMainThread(std::function<void()> fn);
+NAN_METHOD(PollEvents);
 
 }
 
--- deps/exokit-bindings/browser/src/browser-android.cpp
+++ deps/exokit-bindings/browser/src/browser-android.cpp
@@ -8,40 +8,187 @@ extern jobject androidJniContext;
 
 namespace browser {
 
-Browser::Browser(WebGLRenderingContext *gl, int width, int height, const std::string &urlString) {
-  {
-    windowsystem::SetCurrentWindowContext(gl->windowHandle);
+const char *renderVsh = ""
+"#version 300 es\n"
+"\n\
+in vec2 position;\n\
+in vec2 uv;\n\
+out vec2 vUv;\n\
+\n\
+void main() {\n\
+  vUv = uv;\n\
+  gl_Position = vec4(position.xy, 0., 1.);\n\
+}\n\
+";
+const char *renderFsh = ""
+"#version 300 es\n"
+"#extension GL_OES_EGL_image_external_essl3 : require\n"
+"\n\
+in vec2 vUv;\n\
+uniform samplerExternalOES externalTex;\n\
+out vec4 fragColor;\n\
+\n\
+void main() {\n\
+  fragColor = texture(externalTex, vUv);\n\
+}\n\
+";
 
-    glGenTextures(1, &tex);
+Browser::Browser(GLuint externalTex, GLuint tex, int width, int height, const std::string &urlString) : externalTex(externalTex), tex(tex) {
+  JNIEnv *env = androidJniEnv;
+  jobject context = androidJniContext;
 
-    glBindTexture( GL_TEXTURE_EXTERNAL_OES, tex );
-    // Notice the use of GL_TEXTURE_2D for texture creation
-    glTexImage2D( GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, 0 );
-    glTexParameteri( GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );
-    glTexParameteri( GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );
-    glTexParameteri( GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
-    glTexParameteri( GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
-    glBindTexture( GL_TEXTURE_EXTERNAL_OES, 0 );
+  jobject nativeActivity = androidApp->activity->clazz;
+  jclass acl = env->GetObjectClass(nativeActivity);
+  jmethodID getClassLoader = env->GetMethodID(acl, "getClassLoader", "()Ljava/lang/ClassLoader;");
+  jobject cls = env->CallObjectMethod(nativeActivity, getClassLoader);
+  jclass classLoader = env->FindClass("java/lang/ClassLoader");
+  jmethodID findClass = env->GetMethodID(classLoader, "loadClass", "(Ljava/lang/String;)Ljava/lang/Class;");
+  jstring strClassName = env->NewStringUTF("com/webmr/exokit/ExokitWebView");
+  jclass exokitWebViewClass = (jclass)(env->CallObjectMethod(cls, findClass, strClassName));
+
+  jmethodID makeFnId = env->GetStaticMethodID(exokitWebViewClass, "make", "(Landroid/app/Activity;Landroid/content/Context;IIILjava/lang/String;)Lcom/webmr/exokit/ExokitWebView;");
+  jmethodID drawFnId = env->GetMethodID(exokitWebViewClass, "draw", "()V");
+  // jmethodID updateTexImageFnId = env->GetMethodID(exokitWebViewClass, "updateTexImage", "()V");
+  jint externalTexInt = externalTex;
+  jstring url = env->NewStringUTF(urlString.c_str());
+  jobject exokitWebView = env->CallStaticObjectMethod(exokitWebViewClass, makeFnId, androidApp->activity->clazz, context, width, height, externalTexInt, url);
+
+  glGenFramebuffers(1, &renderFbo);
+  glBindFramebuffer(GL_DRAW_FRAMEBUFFER, renderFbo);
+  glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, tex, 0);
+
+  glGenVertexArrays(1, &renderVao);
+
+  // vertex array
+  glBindVertexArray(renderVao);
+
+  // vertex shader
+  renderVertex = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(renderVertex, 1, &renderVsh, NULL);
+  glCompileShader(renderVertex);
+  GLint success;
+  glGetShaderiv(renderVertex, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    char infoLog[4096];
+    GLsizei length;
+    glGetShaderInfoLog(renderVertex, sizeof(infoLog), &length, infoLog);
+    infoLog[length] = '\0';
+    exout << "render vertex shader compilation failed:\n" << infoLog << std::endl;
+    return;
+  };
+
+  // fragment shader
+  renderFragment = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(renderFragment, 1, &renderFsh, NULL);
+  glCompileShader(renderFragment);
+  glGetShaderiv(renderFragment, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    char infoLog[4096];
+    GLsizei length;
+    glGetShaderInfoLog(renderFragment, sizeof(infoLog), &length, infoLog);
+    infoLog[length] = '\0';
+    exout << "render fragment shader compilation failed:\n" << infoLog << std::endl;
+    return;
+  };
+
+  // shader program
+  renderProgram = glCreateProgram();
+  glAttachShader(renderProgram, renderVertex);
+  glAttachShader(renderProgram, renderFragment);
+  glLinkProgram(renderProgram);
+  glGetProgramiv(renderProgram, GL_LINK_STATUS, &success);
+  if (!success) {
+    char infoLog[4096];
+    GLsizei length;
+    glGetShaderInfoLog(renderProgram, sizeof(infoLog), &length, infoLog);
+    infoLog[length] = '\0';
+    exout << "render program linking failed\n" << infoLog << std::endl;
+    return;
   }
 
-  {
-    JNIEnv *env = androidJniEnv;
-    jobject context = androidJniContext;
-
-    jclass exokitWebViewClass = env->FindClass("com/webmr/ExokitWebView");
-    // jmethodID constructor = env->GetMethodID(exokitWebViewClass, "<init>", "(Landroid/content/Context;)V");
-    // jobject exokitWebView = env->NewObject(exokitWebViewClass, constructor, context);
-    jmethodID makeFnId = env->GetStaticMethodID(exokitWebViewClass, "make", "(Landroid/app/Activity;Landroid/content/Context;IIILjava/lang/String;)Lcom/webmr/exokit/ExokitWebView;");
-    jint colorTex = 0;
-    jstring url = env->NewStringUTF(urlString.c_str());
-    jobject exokitWebView = env->CallStaticObjectMethod(exokitWebViewClass, makeFnId, androidApp->activity->clazz, context, width, height, colorTex, url);
-    (void)exokitWebView; // XXX
+  positionLocation = glGetAttribLocation(renderProgram, "position");
+  if (positionLocation == -1) {
+    exout << "render program failed to get attrib location for 'position'" << std::endl;
+    return;
   }
+  GLuint uvLocation = glGetAttribLocation(renderProgram, "uv");
+  if (uvLocation == -1) {
+    exout << "render program failed to get attrib location for 'uv'" << std::endl;
+    return;
+  }
+  GLuint externalTexLocation = glGetUniformLocation(renderProgram, "externalTex");
+  if (externalTexLocation == -1) {
+    exout << "render program failed to get uniform location for 'externalTex'" << std::endl;
+    return;
+  }
+
+  // delete the shaders as they're linked into our program now and no longer necessary
+  glDeleteShader(renderVertex);
+  glDeleteShader(renderFragment);
+
+  GLuint positionBuffer;
+  glGenBuffers(1, &positionBuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, positionBuffer);
+  static const float positions[] = {
+    -1.0f, 1.0f,
+    1.0f, 1.0f,
+    -1.0f, -1.0f,
+    1.0f, -1.0f,
+  };
+  glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+  glEnableVertexAttribArray(positionLocation);
+  glVertexAttribPointer(positionLocation, 2, GL_FLOAT, false, 0, 0);
+
+  GLuint uvBuffer;
+  glGenBuffers(1, &uvBuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, uvBuffer);
+  static const float uvs[] = {
+    0.0f, 0.0f,
+    1.0f, 0.0f,
+    0.0f, 1.0f,
+    1.0f, 1.0f,
+  };
+  glBufferData(GL_ARRAY_BUFFER, sizeof(uvs), uvs, GL_STATIC_DRAW);
+  glEnableVertexAttribArray(uvLocation);
+  glVertexAttribPointer(uvLocation, 2, GL_FLOAT, false, 0, 0);
+
+  GLuint indexBuffer;
+  glGenBuffers(1, &indexBuffer);
+  static const uint16_t indices[] = {0, 2, 1, 2, 3, 1};
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  persistentMainThreadFns.push_back([this, env, exokitWebView, drawFnId, /* updateTexImageFnId, */externalTex, tex, width, height, externalTexLocation]() -> void {
+    env->CallVoidMethod(exokitWebView, drawFnId);
+    // env->CallVoidMethod(exokitWebView, updateTexImageFnId);
+
+    glBindFramebuffer(GL_DRAW_FRAMEBUFFER, this->renderFbo);
+
+    glBindVertexArray(this->renderVao);
+    glUseProgram(this->renderProgram);
+
+    glActiveTexture(GL_TEXTURE0);
+    // glBindTexture(GL_TEXTURE_2D, 0);
+    glBindTexture(GL_TEXTURE_EXTERNAL_OES, externalTex);
+    glUniform1i(externalTexLocation, 0);
+
+    glViewport(0, 0, width, height);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
+
+    glBindTexture(GL_TEXTURE_EXTERNAL_OES, 0);
+
+  });
+}
+Browser::~Browser() {
+  glDeleteFramebuffers(1, &renderFbo);
+  glDeleteVertexArrays(1, &renderVao);
+  glDeleteProgram(renderProgram);
 }
 
-Browser::~Browser() {}
+BrowserWrap::BrowserWrap(Browser *browser) : browser(browser) {}
+BrowserWrap::~BrowserWrap() {}
 
-NAN_METHOD(Browser::New) {
+NAN_METHOD(BrowserWrap::New) {
   if (
     info[0]->IsObject() &&
     info[1]->IsNumber() &&
@@ -54,27 +201,54 @@ NAN_METHOD(Browser::New) {
     Nan::Utf8String urlValue(Local<String>::Cast(info[3]));
     std::string url(*urlValue, urlValue.length());
 
-    Browser *browser = new Browser(gl, width, height, url);
+    GLuint externalTex;
+    GLuint tex;
+    {
+      windowsystem::SetCurrentWindowContext(gl->windowHandle);
+
+      glGenTextures(1, &externalTex);
+
+      glBindTexture( GL_TEXTURE_EXTERNAL_OES, externalTex );
+      glTexParameteri( GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );
+      glTexParameteri( GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );
+      glTexParameteri( GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
+      glTexParameteri( GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
+      glBindTexture( GL_TEXTURE_EXTERNAL_OES, 0 );
+
+      glGenTextures(1, &tex);
+      glBindTexture( GL_TEXTURE_2D, tex );
+      glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );
+      glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );
+      glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+      glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+      glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0 );
+      glBindTexture( GL_TEXTURE_2D, 0 );
+    }
+
+    Browser *browser;
+    RunInMainThread([&]() -> void {
+      browser = new Browser(externalTex, tex, width, height, url);
+    });
+    BrowserWrap *browserWrap = new BrowserWrap(browser);
     Local<Object> browserObj = info.This();
-    browser->Wrap(browserObj);
-    
+    browserWrap->Wrap(browserObj);
     Nan::SetAccessor(browserObj, JS_STR("texture"), TextureGetter);
 
     return info.GetReturnValue().Set(browserObj);
   } else {
-    return Nan::ThrowError("Browser::New: invalid arguments");
+    return Nan::ThrowError("BrowserWrap::New: invalid arguments");
   }
 }
 
-NAN_GETTER(Browser::TextureGetter) {
-  Browser *browser = ObjectWrap::Unwrap<Browser>(info.This());
+NAN_GETTER(BrowserWrap::TextureGetter) {
+  BrowserWrap *browserWrap = ObjectWrap::Unwrap<BrowserWrap>(info.This());
 
   Local<Object> textureObj = Nan::New<Object>();
-  textureObj->Set(JS_STR("id"), JS_INT(browser->tex));
+  textureObj->Set(JS_STR("id"), JS_INT(browserWrap->browser->tex));
   info.GetReturnValue().Set(textureObj);
 }
 
-Local<Object> Browser::Initialize(Isolate *isolate) {
+Local<Object> BrowserWrap::Initialize(Isolate *isolate) {
   Nan::EscapableHandleScope scope;
 
   // constructor
@@ -87,9 +261,38 @@ Local<Object> Browser::Initialize(Isolate *isolate) {
   // Nan::SetMethod(proto, "resize", Resize);
 
   Local<Function> ctorFn = Nan::GetFunction(ctor).ToLocalChecked();
-  // Nan::SetMethod(ctorFn, "updateAll", UpdateAll);
+  Nan::SetMethod(ctorFn, "pollEvents", PollEvents);
 
   return scope.Escape(ctorFn);
+}
+
+std::vector<std::function<void()>> mainThreadFns;
+std::vector<std::function<void()>> persistentMainThreadFns;
+void RunInMainThread(std::function<void()> fn) {
+  uv_sem_t sem;
+  uv_sem_init(&sem, 0);
+
+  mainThreadFns.push_back([&]() -> void {
+    fn();
+
+    uv_sem_post(&sem);
+  });
+
+  uv_sem_wait(&sem);
+  uv_sem_destroy(&sem);
+}
+NAN_METHOD(PollEvents) {
+  std::vector<std::function<void()>> localMainThreadFns = std::move(mainThreadFns);
+  mainThreadFns.clear();
+  for (auto iter = localMainThreadFns.begin(); iter != localMainThreadFns.end(); iter++) {
+    std::function<void()> &fn = *iter;
+    fn();
+  }
+
+  for (auto iter = persistentMainThreadFns.begin(); iter != persistentMainThreadFns.end(); iter++) {
+    std::function<void()> &fn = *iter;
+    fn();
+  }
 }
 
 }
