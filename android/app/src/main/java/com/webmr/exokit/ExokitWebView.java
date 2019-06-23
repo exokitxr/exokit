49c30b936b8630350fbf81dec2720f625819fe48
Avaer Kazmer <a@modules.io>
2019-06-23 09:53:38 -0400

    Add android java initial keyboard/mouse events handling

--- android/app/src/main/java/com/webmr/exokit/ExokitWebView.java
+++ android/app/src/main/java/com/webmr/exokit/ExokitWebView.java
@@ -1,11 +1,13 @@
 package com.webmr.exokit;
 
+import java.text.MessageFormat;
 import android.app.Activity;
 import android.content.Context;
 import android.widget.RelativeLayout;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.webkit.WebChromeClient;
+import android.webkit.ValueCallback;
 import android.graphics.SurfaceTexture;
 import android.graphics.Canvas;
 import android.graphics.Bitmap;
@@ -13,15 +15,18 @@ import android.view.Surface;
 import android.view.ViewGroup;
 import android.os.Looper;
 import android.os.Handler;
+import android.view.MotionEvent;
 
 public class ExokitWebView extends WebView
 {
+    private Activity _activity = null;
     private Surface _webViewSurface = null;
     private SurfaceTexture _webViewSurfaceTexture = null;
 
-    public ExokitWebView(Context context, int width, int height, int colorTex, Surface webviewSurface, SurfaceTexture webViewSurfaceTexture, String url) {
+    public ExokitWebView(Activity activity, Context context, int width, int height, int colorTex, Surface webviewSurface, SurfaceTexture webViewSurfaceTexture, String url) {
       super(context);
 
+      _activity = activity;
       _webViewSurface = webviewSurface;
       _webViewSurfaceTexture = webViewSurfaceTexture;
 
@@ -72,7 +77,7 @@ public class ExokitWebView extends WebView
         public volatile ExokitWebView result = null;
         public void run() {
           try {
-            ExokitWebView webView = new ExokitWebView(context, width, height, colorTex, webViewSurface, webViewSurfaceTexture, url);
+            ExokitWebView webView = new ExokitWebView(activity, context, width, height, colorTex, webViewSurface, webViewSurfaceTexture, url);
 
             activity.addContentView(webView, new RelativeLayout.LayoutParams(width, height));
 
@@ -133,6 +138,74 @@ public class ExokitWebView extends WebView
       } catch (Exception err) {
         err.printStackTrace();
       }
+    }
+
+    public void runJs(String jsString) {
+      _activity.runOnUiThread(new Runnable() {
+        public void run() {
+          evaluateJavascript(jsString, new ValueCallback<String>() {
+            public void onReceiveValue(String value) {
+              // nothing
+            }
+          });
+        }
+      });
+    }
+
+    public void keyDown(int keyCode) {
+      Object[] params = new Object[]{String.valueOf(keyCode)};
+      String jsString = MessageFormat.format("window.dispatchEvent(new KeyboardEvent('keydown', '{'keyCode: {0}'}'))", params);
+
+      runJs(jsString);
+    }
+
+    public void keyUp(int keyCode) {
+      Object[] params = new Object[]{String.valueOf(keyCode)};
+      String jsString = MessageFormat.format("window.dispatchEvent(new KeyboardEvent('keyup', '{'keyCode: {0}'}'))", params);
+
+      runJs(jsString);
+    }
+
+    public void keyPress(int keyCode) {
+      Object[] params = new Object[]{String.valueOf(keyCode)};
+      String jsString = MessageFormat.format("window.dispatchEvent(new KeyboardEvent('keypress', '{'keyCode: {0}'}'))", params);
+
+      runJs(jsString);
+    }
+
+    public void mouseDown(int x, int y, int button) {
+      Object[] params = new Object[]{String.valueOf(x), String.valueOf(y), String.valueOf(button)};
+      String jsString = MessageFormat.format("window.dispatchEvent(new MouseEvent('mousedown', '{'clientX: {0}, clientY: {1}, button: {2}'}'))", params);
+
+      runJs(jsString);
+    }
+
+    public void mouseUp(int x, int y, int button) {
+      Object[] params = new Object[]{String.valueOf(x), String.valueOf(y), String.valueOf(button)};
+      String jsString = MessageFormat.format("window.dispatchEvent(new MouseEvent('mouseup', '{'clientX: {0}, clientY: {1}, button: {2}'}'))", params);
+
+      runJs(jsString);
+    }
+
+    public void click(int x, int y, int button) {
+      Object[] params = new Object[]{String.valueOf(x), String.valueOf(y), String.valueOf(button)};
+      String jsString = MessageFormat.format("window.dispatchEvent(new MouseEvent('click', '{'clientX: {0}, clientY: {1}, button: {2}'}'))", params);
+
+      runJs(jsString);
+    }
+
+    public void mouseMove(int x, int y) {
+      Object[] params = new Object[]{String.valueOf(x), String.valueOf(y)};
+      String jsString = MessageFormat.format("window.dispatchEvent(new MouseEvent('mousemove', '{'clientX: {0}, clientY: {1}'}'))", params);
+
+      runJs(jsString);
+    }
+
+    public void mouseWheel(int deltaX, int deltaY) {
+      Object[] params = new Object[]{String.valueOf(deltaX), String.valueOf(deltaY)};
+      String jsString = MessageFormat.format("window.dispatchEvent(new WheelEvent('wheel', '{'deltaX: {1}, deltaY: {2}'}'))", params);
+
+      runJs(jsString);
     }
 
 }
