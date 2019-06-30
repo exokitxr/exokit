9c89a5556e1a91568c6d189fc242cf376fcc45e6
RangerMauve <ranger@mauve.moe>
2019-06-30 14:45:57 -0400

    Clean up protocol handling code

--- src/fetch.js
+++ src/fetch.js
@@ -1,13 +1,27 @@
-const windowFetch = require('window-fetch');
-const protoFetch = require('proto-fetch')
+const windowFetch = require('window-fetch')
+const url = require('url')
+
+// This error message is what Firefox says when you fetch an unknown protocol
+const INVALID_PROTOCOL = 'NetworkError when attempting to fetch resource.'
 
 const protocols = {
   file: windowFetch,
   http: windowFetch,
-  https: windowFetch
+  https: windowFetch,
+  data: windowFetch,
+  blob: windowFetch
 }
 
-const fetch = protoFetch(protocols)
+const fetch = async function(uri, options) {
+  const parsed = url.parse(uri)
+  const protocol = parsed.protocol.split(':')[0]
+
+  if(!protocols[protocol]) {
+    return new Promise.reject(new TypeError(INVALID_PROTOCOL))
+  }
+
+  return protocols[protocol](uri, options)
+}
 
 const {Request, Response, Headers, Blob} = windowFetch
 
