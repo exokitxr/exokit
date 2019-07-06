2bf8a9d09133e9dd4fdb2738c882663b7a2af5fe
Avaer Kazmer <a@modules.io>
2019-07-06 02:37:59 -0400

    Update openvr action state per frame

--- deps/openvr/src/ivrcompositor.cpp
+++ deps/openvr/src/ivrcompositor.cpp
@@ -270,6 +270,8 @@ NAN_METHOD(IVRCompositor::RequestGetPoses) {
       TrackedDevicePoseArray trackedDevicePoseArray;
 	    obj->self_->WaitGetPoses(trackedDevicePoseArray.data(), static_cast<uint32_t>(trackedDevicePoseArray.size()), nullptr, 0);
 
+      vr::VRInput()->UpdateActionState();
+
       const float identityMatrix[] = {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1};
       memcpy(hmdArray, identityMatrix, sizeof(identityMatrix));
       memcpy(leftControllerArray, identityMatrix, sizeof(identityMatrix));
