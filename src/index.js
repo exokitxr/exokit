7dd5b581ec4a8e0021ce9e541753eeaa2380d3d5
Avaer Kazmer <a@modules.io>
2019-07-10 16:53:40 -0400

    Remove dead index.js getContext call

--- src/index.js
+++ src/index.js
@@ -444,7 +444,6 @@ const _waitHandleRequest = ({type, keypath}) => {
       xrState.renderWidth[0] = halfWidth;
       xrState.renderHeight[0] = height;
     } else if (hmdType === 'openvr') {
-      const vrContext = nativeBindings.nativeOpenVR.getContext();
       const system = nativeBindings.nativeOpenVR.VR_Init(nativeBindings.nativeOpenVR.EVRApplicationType.Scene, path.join(__dirname, '..', 'deps', 'openvr', 'actions.json'));
       const compositor = nativeBindings.nativeOpenVR.NewCompositor();
       // const lmContext = topVrPresentState.lmContext || (nativeLm && new nativeLm());
