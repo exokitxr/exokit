bff823b560e8fa576cd5f12a5c42cb515b985f11
Avaer Kazmer <a@modules.io>
2019-07-24 01:24:22 -0400

    Small script cleanup

--- scripts/oculusmobile/run-android.sh
+++ scripts/oculusmobile/run-android.sh
@@ -15,6 +15,6 @@ pushd ../../android
 "$ANDROID_HOME/platform-tools/adb" shell am force-stop com.webmr.exokit
 "$ANDROID_HOME/platform-tools/adb" logcat -c
 "$ANDROID_HOME/platform-tools/adb" shell am start -n com.webmr.exokit/android.app.NativeActivity
-# "$ANDROID_HOME/platform-tools/adb" shell am start -n com.webmr.exokit/android.app.NativeActivity -e ARGS "'node --inspect-brk /package /package/examples/tutorial.html'"
+# "$ANDROID_HOME/platform-tools/adb" shell am start -n com.webmr.exokit/android.app.NativeActivity -e ARGS "'node /package /package/examples/tutorial.html'"
 "$ANDROID_HOME/platform-tools/adb" logcat
 popd
