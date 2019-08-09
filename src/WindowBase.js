0c78f758457ec02b66fda72c5c452c8395d2ec67
Avaer Kazmer <a@modules.io>
2019-08-09 01:15:30 -0400

    Pass in missing importScripts baseUrl

--- src/WindowBase.js
+++ src/WindowBase.js
@@ -281,7 +281,7 @@ function getScript(url) {
 function importScripts() {
   for (let i = 0; i < arguments.length; i++) {
     const importScriptPath = arguments[i];
-    const importScriptSource = getScript(importScriptPath);
+    const importScriptSource = getScript(importScriptPath, args.options.baseUrl);
     vm.runInThisContext(importScriptSource, global, {
       filename: /^https?:/.test(importScriptPath) ? importScriptPath : 'data-url://',
     });
