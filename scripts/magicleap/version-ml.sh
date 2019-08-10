921e7cb6198f2ee9cf2ff58951ea26c06c3a3df8
chrisplatorres <me@chrislatorres.com>
2019-08-10 17:31:04 -0500

    revert chris to avaer in version-ml path

--- scripts/magicleap/version-ml.sh
+++ scripts/magicleap/version-ml.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export MLSDK=${MLSDK:-/mnt/c/Users/chris/MagicLeap/mlsdk/v0.19.0}
+export MLSDK=${MLSDK:-/mnt/c/Users/avaer/MagicLeap/mlsdk/v0.19.0}
 export MLSDK_WIN=$(echo "$MLSDK" | sed 's/^\/mnt\/\([a-z]\)\//\1:\\/' | sed 's/\//\\/g')
